a6adb96512d49984cc4b60a37310fcbc01fd28fc
Pascal P <lacsap_666@yahoo.fr>
2020-08-13 23:05:57 +1200

    review hamming exo

--- hamming/hamming.jl
+++ hamming/hamming.jl
@@ -1,6 +1,6 @@
 """
-Calcluation hamming distance between 2 DNA-String (aka strand)
-of equals length
+Calculate Hamming distance between 2 DNA-String (aka strand)
+of equals length. Assuming both strand are valid
 
 E.g.
   GAGCCTACTAACGGGAT
@@ -8,19 +8,13 @@ E.g.
   ^ ^ ^  ^ ^    ^^
 
   distance("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT") == 7
-
 """
 
-function distance(a::String, b::String)::Int
-
+function distance(a::String, b::String)::Integer
   length(a) != length(b) && throw(ArgumentError("strand must have the same length"))
-
   a == b && return 0  # encompasses # a == "" && b == "" && return 0
 
-  dist = 0
-  for (ch1, ch2) in zip(a, b)
-    ch1 != ch2 && (dist += 1)
-  end
-
-  return dist
+  return reduce(+,
+                map((tch) -> tch[1] == tch[2] ? 0 : 1, zip(a, b));
+                init=0)
 end
