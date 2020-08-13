6785b7ac9f0329ec16ca6fdccf3b48f4d800cb06
Pascal P <lacsap_666@yahoo.fr>
2020-08-13 22:04:36 +1200

    difference-of-squares - using supertype Integer

--- difference-of-squares/difference-of-squares.jl
+++ difference-of-squares/difference-of-squares.jl
@@ -1,43 +1,38 @@
 """
   Difference of Square
-
-  Addressing some Integer Overflow
+  Addressing Integer Overflow
 """
 
-YaInt = Union{Integer, Unsigned, BigInt}
-
 "Square the sum of the first `n` positive integers"
-function square_of_sum(n::YaInt)::YaInt
+function square_of_sum(n::Integer)::Integer
   # sum(1:n)^2 ≡ (n × (n + 1) / 2)²
   n = _promote(n)
-
   x₁ = n
   x₂ = x₁ + 1
   x = (x₁ * x₂) >> 1
-  x * x
+  return x * x
 end
 
 "Sum the squares of the first `n` positive integers"
-function sum_of_squares(n::YaInt)::YaInt
+function sum_of_squares(n::Integer)::Integer
+  # 1² + 2² + 3² + ... + n² ≡ (n × (n + 1) × (2n + 1)) / 6
   n = _promote(n)
-
   x₁ = n
   x₂ = x₁ + 1
   x₃ = (n << 1) + 1
   x = (x₁ * x₂ * x₃) >> 1
-  div(x, 3)
+  return div(x, 3)
 end
 
 "Subtract the sum of squares from square of the sum of the first `n` positive ints"
-function difference(n::YaInt)::YaInt
+function difference(n::Integer)::Integer
   n = _promote(n)
-
-  square_of_sum(n) - sum_of_squares(n)
+  return square_of_sum(n) - sum_of_squares(n)
 end
 
 
-function _promote(n::YaInt)::YaInt
-  "Promote to Unsigned or BigInt"
+function _promote(n::Integer)::Integer
+  "Promote fromn Signed Ints to Unsigned or BigInt"
   if typeof(n) ∈ (Int8, Int16, Int32, Int64)
     n = UInt(n)
   elseif isa(n, Int128)
@@ -45,5 +40,5 @@ function _promote(n::YaInt)::YaInt
   elseif typeof(n) ∈ [Unsigned, BigInt]
     # nothing
   end
-  n
+  return n
 end
