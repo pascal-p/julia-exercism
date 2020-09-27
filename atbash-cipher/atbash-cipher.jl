8c7a67c8f949ea390ae6c0e66f6fe69dd7ba0adf
Pascal P <lacsap_666@yahoo.fr>
2020-09-27 17:16:32 +1300

    atbash-cipher review

--- atbash-cipher/atbash-cipher.jl
+++ atbash-cipher/atbash-cipher.jl
@@ -9,9 +9,9 @@
   - Grouping by 5
 """
 
-const GROUP_FACT = 5
+const GROUP_FACT = 5 + 1  # counting 1 extra space
 
-function encode(input::AbstractString; enc=true)::AbstractString
+function encode_v1(input::AbstractString; enc=true)::AbstractString
   s = " "
 
   for ch in input
@@ -22,6 +22,19 @@ function encode(input::AbstractString; enc=true)::AbstractString
   end
 
   return strip(s, ' ')
+end
+
+function encode(input::AbstractString; enc=true)::AbstractString
+  trans_fn = function(s, ch)
+    !occursin(r"[a-zA-Z0-9]+", string(ch)) && (return s)
+
+    s = '0' ≤ ch ≤ '9' ? string(s, ch) : string(s, 'z' - lowercase(ch) + 'a')
+    enc && length(s) % GROUP_FACT == 0 ? string(s, ' ') : s
+  end
+
+  foldl((s, ch) -> trans_fn(s, ch),
+        collect(input),
+        init=" ") |> s -> strip(s, ' ')
 end
 
 decode(input::AbstractString)::AbstractString = encode(input; enc=false)
