30a4c38b81715dd2bb30813444c4d85fc72d213e
Pascal P <lacsap_666@yahoo.fr>
2020-09-28 19:51:27 +1300

    etl review

--- etl/etl.jl
+++ etl/etl.jl
@@ -2,7 +2,7 @@
 Assume input contains only valid score with defined letters from 'A'..'Z'
 """
 
-function transform(input::AbstractDict)
+function transform_v1(input::AbstractDict)::Dict{AbstractChar, Int}
   ninput = Dict{AbstractChar, Int}()
 
   for (k, vs) in input
@@ -10,8 +10,22 @@ function transform(input::AbstractDict)
     # ninput = merge(ninput, Dict(lowercase(v) => k for v in vs))
 
     ## more efficient
-    for v in vs; ninput[lowercase(v)] = k; end  ## for v in lowercase.(vs); ninput[v] = k; end
+    ## for v in lowercase.(vs); ninput[v] = k; end
+    for v in vs; ninput[lowercase(v)] = k; end
   end
 
   return ninput
+end
+
+function transform(input::AbstractDict)::Dict{AbstractChar, Int}
+
+  λ = function(hsh, (k, vs))
+    foldl((nhsh, v) -> (nhsh[lowercase(v)] = k; nhsh), vs, init=hsh)
+
+    ## or:
+    #for v in vs; hsh[lowercase(v)] = k; end
+    #hsh
+  end
+
+  return foldl(λ, input, init=Dict{AbstractChar, Int}())
 end
