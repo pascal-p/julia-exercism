01d503f900740484f9e02a9ceebe8d357a8b86e6
Pascal P <lacsap_666@yahoo.fr>
2020-11-29 23:16:30 +1300

    08-heap revision: kv pair

--- Algo/08-heap/runtests_heap.jl
+++ Algo/08-heap/runtests_heap.jl
@@ -6,52 +6,54 @@ using YAH
 
 @testset "min heap basics" begin
   n = 10
-  mheap = Heap{Int}(n)  # MinHeap default
+  mheap = Heap{Int, Float64}(n)  # MinHeap default
 
   @test size(mheap) == n
   @test isempty(mheap)
 
-  insert!(mheap, 21)
+  insert!(mheap, (key=21, value=1.1))
   @test mheap.last == 2  ## next free position
 end
 
 @testset "min heap insert" begin
   n = 2
-  mheap = Heap{Int}(n)
+  mheap = Heap{Int, Float64}(n)
 
-  for x in [14, 12, 10]
+  for x in [(key=14, value=2.0), (key=12, value=π/2), (key=10, value=ℯ/2)]
     insert!(mheap, x)
     @test peek(mheap) == x
   end
 
   @test size(mheap) == 2n   ## should have double
-  @test mheap.last == 4
+  @test mheap.last[1] == 4
 
 end
 
 @testset "min heapify" begin
-  x = [35, 33, 10, 45, 30, 49, 48, 34, 12, 5, 14, 43, 16, 24, 31]
-  mheap = Heap{Int}(15)
+  x = [(key=35, value=2.0), (key=33, value=1.1), (key=10, value=√2), (key=45, value=√3), (key=30, value=ℯ/2), (key=49, value=ℯ/3), (key=48, value=π/4), (key=34, value=π/2), (key=12, value=√5), (key=5, value=-√5), (key=14, value=π/12), (key=43, value=log(2.1)), (key=16, value=5.0), (key=24, value=11.3), (key=31, value=√2/2)]
+  mheap = Heap{Int, Float64}(15)
   heapify!(mheap, x)
 
-  @test mheap.h == [5, 12, 10, 34, 14, 16, 24, 35, 45, 30, 33, 43, 49, 48, 31]
+  @test map(t -> t[1],
+            mheap.h) == [5, 12, 10, 34, 14, 16, 24, 35, 45, 30, 33, 43, 49, 48, 31]
 end
 
 @testset "max heapify" begin
-  x = [35, 33, 10, 45, 30, 49, 48, 34, 12, 5, 14, 43, 16, 24, 31]
-  mheap = Heap{Int}(15; klass=MaxHeap)
+  x = [(key=35, value=2.0), (key=33, value=1.1), (key=10, value=√2), (key=45, value=√3), (key=30, value=ℯ/2), (key=49, value=ℯ/3), (key=48, value=π/4), (key=34, value=π/2), (key=12, value=√5), (key=5, value=-√5), (key=14, value=π/12), (key=43, value=log(2.1)), (key=16, value=5.0), (key=24, value=11.3), (key=31, value=√2/2)]
+  mheap = Heap{Int, Float64}(15; klass=MaxHeap)
   heapify!(mheap, x)
 
-  @test mheap.h == [49, 45, 48, 34, 30, 43, 35, 33, 12, 5, 14, 10, 16, 24, 31]
+  @test map(t -> t[1],
+            mheap.h) == [49, 45, 48, 34, 30, 43, 35, 33, 12, 5, 14, 10, 16, 24, 31]
 end
 
 @testset "max heap insert" begin
   n = 2
-  mheap = Heap{Int}(n; klass=MaxHeap) # :maxheap)
+  mheap = Heap{Int, Float64}(n; klass=MaxHeap) # :maxheap)
 
-  for x in [10, 12, 14,]    ## contrived example
+  for x in [(key=10, value=ℯ/2), (key=12, value=π/3), (key=14, value=2.0)]    ## contrived example
     insert!(mheap, x)
-    @test peek(mheap) == x
+    @test peek(mheap)[1] == x[1]
   end
 
   @test size(mheap) == 2n   ## should have double
@@ -61,8 +63,8 @@ end
 
 @testset "min heap extract_min" begin
   n = 4
-  ary = [14, 16, 10, 12, 15]
-  mheap = Heap{Int}(n)
+  ary = [(key=14, value=√2), (key=16, value=ℯ/2), (key=10, value=π/4), (key=12, value=√5), (key=15, value=π/12)]
+  mheap = Heap{Int, Float64}(n)
 
   for x in ary
     insert!(mheap, x)
@@ -73,27 +75,27 @@ end
 
   x = extract_min!(mheap)
   @test x == minimum(ary) # == 10
-  @test peek(mheap) == 12
+  @test peek(mheap)[1] == 12
 
-  x = extract_min!(mheap)
+  x = extract_min!(mheap)[1]
   @test x == 12
-  @test peek(mheap) == 14
+  @test peek(mheap)[1] == 14
 
-  x = extract_min!(mheap)
+  x = extract_min!(mheap)[1]
   @test x == 14
-  @test peek(mheap) == 15
+  @test peek(mheap)[1] == 15
 
-  x = extract_min!(mheap)
+  x = extract_min!(mheap)[1]
   @test x == 15
-  @test peek(mheap) == 16
+  @test peek(mheap)[1] == 16
 
   @test size(mheap) == floor(Int, 2n * 0.75)  ## should have shrinked
 end
 
 @testset "max heap extract_max" begin
   n = 4
-  ary = [14, 16, 10, 12, 15]
-  mheap = Heap{Int}(n, klass=MaxHeap) # :maxheap)
+  ary = [(key=14, value=√2), (key=16, value=ℯ/2), (key=10, value=π/4), (key=12, value=√5), (key=15, value=π/12)]
+  mheap = Heap{Int, Float64}(n, klass=MaxHeap) # :maxheap)
 
   for x in ary
     insert!(mheap, x)
@@ -102,63 +104,65 @@ end
   @test peek(mheap) == maximum(ary)
   @test size(mheap) == 2n   ## should have double
 
-  x = extract_max!(mheap)
-  @test x == maximum(ary) # == 16
-  @test peek(mheap) == 15
+  x = extract_max!(mheap)[1]
+  @test x == maximum(ary)[1] # == 16
+  @test peek(mheap)[1] == 15
 
-  x = extract_max!(mheap)
+  x = extract_max!(mheap)[1]
   @test x == 15
-  @test peek(mheap) == 14
+  @test peek(mheap)[1] == 14
 
-  x = extract_max!(mheap)
+  x = extract_max!(mheap)[1]
   @test x == 14
-  @test peek(mheap) == 12
+  @test peek(mheap)[1] == 12
 
-  x = extract_max!(mheap)
+  x = extract_max!(mheap)[1]
   @test x == 12
-  @test peek(mheap) == 10
+  @test peek(mheap)[1] == 10
 
   @test size(mheap) == floor(Int, 2n * 0.75)  ## should have shrinked
 end
 
 @testset "min heap deletion" begin
-  n = 10; mheap = Heap{Int}(n)
-  for x in [21, 10, 14, 14, 13, 12, 15]
+  n = 10; mheap = Heap{Int, Float64}(n)
+  for x in [(key=21, value=√2), (key=10, value=√3), (key=14, value=√7), (key=14, value=√5), (key=13, value=√2), (key=12, value=π/2), (key=15, value=2π)]
     insert!(mheap, x)
   end
 
-  @test delete!(mheap, 2) == 13
+  @test delete!(mheap, 2)[1] == 13
 
-  @test peek(mheap) == 10
+  @test peek(mheap)[1] == 10
   @test mheap.last - 1 == 6
 
   # under the hood
-  @test mheap.h[1:(mheap.last - 1)] == [10, 14, 12, 21, 15, 14]
+  @test map(t -> t[1],
+            mheap.h[1:(mheap.last - 1)]) == [10, 14, 12, 21, 15, 14]
 end
 
 @testset "max heap deletion" begin
-  n = 10; mheap = Heap{Int}(n; klass=MaxHeap) # :maxheap)
-  for x in [21, 10, 14, 14, 13, 12, 15]
+  n = 10; mheap = Heap{Int, Float64}(n; klass=MaxHeap) # :maxheap)
+  for x in [(key=21, value=√2), (key=10, value=√3), (key=14, value=√7), (key=14, value=√5), (key=13, value=√2), (key=12, value=π/2), (key=15, value=2π)]
     insert!(mheap, x)
   end
   # == Heap{Int64}([21, 14, 15, 10, 13, 12, 14, 0, 0, 0], 8, :maxheap)
 
-  @test delete!(mheap, 2) == 14
+  @test delete!(mheap, 2)[1] == 14
 
-  @test peek(mheap) == 21
+  @test peek(mheap)[1] == 21
   @test mheap.last - 1 == 6
 
   # under the hood
-  @test mheap.h[1:(mheap.last - 1)] == [21, 14, 15, 10, 13, 12]
+  @test map(t -> t[1],
+            mheap.h[1:(mheap.last - 1)]) == [21, 14, 15, 10, 13, 12]
 end
 
 
 @testset "exceptions" begin
   n = 2
-  mheap = Heap{Int}(n)
+  mheap = Heap{Int, Int}(n)
 
   @test_throws ArgumentError peek(mheap)          ## empty heap!
   @test_throws ArgumentError extract_min!(mheap)  ## empty heap!
 
-  @test_throws AssertionError Heap{Int}(n; klass=:fooheap)
+  @test_throws AssertionError Heap{Int, Int}(n; klass=:fooheap)
 end
--- Algo/08-heap/src/heap.jl
+++ Algo/08-heap/src/heap.jl
@@ -9,67 +9,74 @@ abstract type AbsHeap end
 abstract type MinHeap <: AbsHeap end
 abstract type MaxHeap <: AbsHeap end
 
-mutable struct Heap{T} <: AbsHeap
-  h::Vector{T}
+const KV{T1, T2} = NamedTuple{(:key, :value), Tuple{T1, T2}} where {T1, T2}
+
+mutable struct Heap{T1, T2} <: AbsHeap
+  h::Vector{KV{T1, T2}}
   last::Int                  ## index to next free position in the heap
   klass::DataType            ## MinHeap or MaxHeap
 
-  function Heap{T}(n::Int=4; klass=MinHeap) where T
+  function Heap{T1, T2}(n::Int=4; klass=MinHeap) where {T1, T2}
     @assert klass == MinHeap || klass == MaxHeap "Either MaxHeap xor MaxHeap"
-    self = new(zeros(T, n), 1, klass)
+    self = new(Vector{KV{T1, T2}}(undef, n), 1, klass)
   end
 end
 
 
 """
   length
-    - Current number of items in the heap
+Current number of items in the heap
 """
-function length(self::Heap{T})::Int where T
-  self.last - 1
-end
+length(self::Heap{T1, T2}) where {T1, T2} = self.last - 1
 
 """
   size
-    - Current (Allocated) size of the heap
+Current (Allocated) size of the heap
 """
-function size(self::Heap{T})::Int where T
-  length(self.h)
-end
+size(self::Heap{T1, T2}) where {T1, T2} = length(self.h)
 
-function isempty(self::Heap{T})::Bool where T
-  length(self.last) == 1
-end
+isempty(self::Heap{T1, T2}) where {T1, T2} = length(self.last) == 1
 
-function peek(self::Heap{T})::T where T
+function peek(self::Heap{T1, T2})::KV{T1, T2} where {T1, T2}
   self.last ≤ 1 && throw(ArgumentError("empty heap!"))
   self.h[1]
 end
 
-function insert!(self::Heap{T}, k::T) where T
+
+"""
+  insert!(self::Heap{T1, T2}, kv::KV{T1, T2})
+
+insert pair kv into the heap self
+"""
+function insert!(self::Heap{T1, T2}, kv::KV{T1, T2}) where {T1, T2}
   self.last > size(self) && _resize!(self)
 
   ## 1 - Stick the new object at the end of the heap and incr. the heap size.
-  self.h[self.last] = k
+  self.h[self.last] = kv
   self.last += 1
 
   ## 2 - Buble-up (swap) repeatedly until heap property is restored
   _buble_up!(self.klass, self)
 end
 
-extract_min!(self::Heap{T}) where T = _extract(self; cmp=(>))
+extract_min!(self::Heap{T1, T2}) where {T1, T2} = _extract(self; cmp=(>))
+extract_max!(self::Heap{T1, T2}) where {T1, T2} = _extract(self; cmp=(<))
 
-extract_max!(self::Heap{T}) where T = _extract(self; cmp=(<))
-
-function heapify!(self::Heap{T}, a::Vector{T}) where T
+function heapify!(self::Heap{T1, T2}, a::Vector{KV{T1, T2}}) where {T1, T2}
   n = length(a)
   self.h, self.last = a, n + 1
+
   for k in n ÷ 2:-1:1
     _buble_down!(self.klass, self, k)
   end
 end
 
-function delete!(self::Heap{T}, k::T)::T where T
+"""
+  delete!(self::Heap{T1, T2}, k::T1)
+
+delete key/value pair located at index k in the heap self
+"""
+function delete!(self::Heap{T1, T2}, k::T1)::KV{T1, T2} where {T1, T2}
   ## 0 - empty?
   self.last ≤ 1 && throw(ArgumentError("empty heap!"))
   k ≤ 1 || k > self.last - 1 && throw(ArgumentError("no such element in the heap!"))
@@ -77,21 +84,21 @@ function delete!(self::Heap{T}, k::T)::T where T
   ## 1 - find element k in heap - trivial as k is the index
   ## delete => replace deleted value with last item in the heap
   ## decr last
-  item = self.h[k]
+  pair = self.h[k]
   self.h[k] = self.h[self.last - 1]
   self.last -= 1
 
   ## 2 - buble-down
   _buble_down!(self.klass, self, k)
 
-  return item
+  return pair
 end
 
 #
 # Internal
 #
 
-function _extract(self::Heap{T}; cmp=self.klass == MinHeap ? (>) : (<))::T where T
+function _extract(self::Heap{T1, T2}; cmp=self.klass == MinHeap ? (>) : (<))::KV{T1, T2} where {T1, T2}
   ## 0. check
   self.last ≤ 1 && throw(ArgumentError("empty heap!"))
   self.last < (size(self) ÷ 2) && _resize!(self; incr=false)
@@ -115,26 +122,25 @@ end
 for (klass, op) in ((MaxHeap , <), (MinHeap, >))
 
   @eval begin
-    function _buble_up!(::Type{$(klass)}, self::Heap{T}) where T
+    function _buble_up!(::Type{$(klass)}, self::Heap{T1, T2}) where {T1, T2}
       k = self.last - 1
 
       while k > 1
         j = k ÷ 2
-
-        ($(op))(self.h[k], self.h[j]) && break
+        ($(op))(self.h[k].key, self.h[j].key) && break
         self.h[k], self.h[j] = self.h[j], self.h[k]
-
         k = j
       end
     end
 
-    function _buble_down!(::Type{$(klass)}, self::Heap{T}, k::T) where T
+    function _buble_down!(::Type{$(klass)}, self::Heap{T1, T2}, k::T1) where {T1, T2}
       n = self.last - 1
 
       while 2k ≤ n
         j = 2k                                                 ## first child position
-        j < n && ($(op))(self.h[j], self.h[j + 1]) && (j += 1) ## move to 2nd child if necessary
-        !($(op))(self.h[k], self.h[j]) && break                ## we are done
+        j < n && ($(op))(self.h[j].key, self.h[j + 1].key) &&
+          (j += 1)                                             ## move to 2nd child if necessary
+        !($(op))(self.h[k].key, self.h[j].key) && break        ## we are done
 
         self.h[k], self.h[j] = self.h[j], self.h[k]            ## exchange
         k = j
@@ -145,11 +151,11 @@ for (klass, op) in ((MaxHeap , <), (MinHeap, >))
 
 end
 
-function _resize!(self::Heap{T}; incr=true) where T
+function _resize!(self::Heap{T1, T2}; incr=true) where {T1, T2}
   n = size(self)
   nsize = incr ? 2n : floor(Int, n * 0.75)
   c_size = min(n, nsize)
-  new_heap = Heap{T}(nsize)
+  new_heap = Heap{T1, T2}(nsize)
   last = self.last
   copyto!(new_heap.h, self.h[1:c_size])
   self.h, self.last = new_heap.h, last
