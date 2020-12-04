683d527c373fa51ed1504684ba0463772fc0b4b4
Pascal P <lacsap_666@yahoo.fr>
2020-12-04 21:51:28 +1300

    14-2-greedy-kruskal_mst: challenge

--- Algo/14-2-greedy-kruskal_mst/runtests_ug_kruskal_uf.jl
+++ Algo/14-2-greedy-kruskal_mst/runtests_ug_kruskal_uf.jl
@@ -33,3 +33,25 @@ for file in filter((fs) -> occursin(r"\Ainput_random_\d+_\d+", fs),
     @test mst(ug)[1] == exp_value
   end
 end
+
+## Challenge / Princeton files
+for file in filter((fs) -> occursin(r".+_ewg-challenge.txt", fs),
+                   cd(readdir, "$(TF_DIR)"))
+
+  ug = from_file("$(TF_DIR)/$(file)", UnGraph{Int, Int}; T1=Int, T2=Int, cache_edges=true)
+  exp_value = occursin(r"large_ewg-challenge.txt", file) ? 64_766_236_568 : 1_046_336
+
+  @testset "(uf) kruskal MST on: $(file)" begin
+    @time @test mst(ug)[1] == exp_value
+  end
+end
+
+# julia runtests_ug_kruskal_uf.jl
+
+#   1.738447 seconds (2.00 M allocations: 461.425 MiB, 1.59% gc time)
+# Test Summary:                                | Pass  Total
+# (uf) kruskal MST on: large_ewg-challenge.txt |    1      1
+
+# 0.000128 seconds (532 allocations: 103.562 KiB)
+# Test Summary:                                 | Pass  Total
+# (uf) kruskal MST on: medium_ewg-challenge.txt |    1      1
