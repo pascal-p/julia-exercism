a9fa1e26c74cf69941bf7419bdf8e2d5a73da56c
Pascal P <lacsap_666@yahoo.fr>
2020-12-20 16:31:14 +1300

    edge weighted digraph: update for Int32/64 Float32/64

--- Algo/07-dijkstra-sp/lib/edge_weighted_di_graph.jl
+++ Algo/07-dijkstra-sp/lib/edge_weighted_di_graph.jl
@@ -4,28 +4,34 @@
   Note: weigths are assumed to be positive integer
 """
 
-mutable struct EWDiGraph{T, T1 <: Real}
-  _v::Int                             # num. of vertices 1..v
-  _e::Int                             # num. of edges
+mutable struct EWDiGraph{T <: Integer, T1 <: Real}
+  _v::Integer                         # num. of vertices 1..v
+  _e::Integer                         # num. of edges
   _adj::Vector{Vector{Tuple{T, T1}}}  # for adjacency list (of tuples/pairs vertex(T), weight(T1)
 
-  function EWDiGraph{T, T1}(v::Int) where {T, T1 <: Real}
+  function EWDiGraph{T, T1}(v::T) where {T <: Integer, T1 <: Real}
     adj = [ Vector{Tuple{T, T1}}() for _ in 1:v ]
     self = new(v, 0, adj)
     return self
   end
 
-  function EWDiGraph{T, T1}(infile::String; positive_weight=true) where {T, T1 <: Real}
+  function EWDiGraph{T, T1}(infile::String; positive_weight=true) where {T <: Integer, T1 <: Real}
     from_file(infile, EWDiGraph{T, T1}, T; WType=T1, positive_weight)
   end
 end
 
-function add_edge(self::EWDiGraph{T, T1}, x::T, y::T, w::T1;
-                  positive_weight=true) where {T, T1 <: Real}
+function add_edge(g::EWDiGraph{T, T1}, x::T, y::T, w::T1;
+                  positive_weight=true) where {T <: Integer, T1 <: Real}
   positive_weight && (@assert w >= 0)
 
-  push!(self._adj[x], (y, w)) ## tuple
-  self._e += 1
+  # if edge (x, y) already defined in graph => parallel edge detected, keep min weight
+  (found, nw, ix) = find_weighted_edge(g, x, y)
+  if found
+    w < nw && (g._adj[x][ix] = (y, w))
+  else
+    push!(g._adj[x], (y, w)) ## tuple
+    g._e += 1
+  end
 end
 
 ##
@@ -37,20 +43,20 @@ e(g::EWDiGraph) = g._e
 
 adj(g::EWDiGraph, u::T) where T = 1 ≤ u ≤ v(g) ? g._adj[u] : Vector{Tuple{T, T1}}()
 
-function has_edge(g::EWDiGraph{T, T1}, u::T, v::T) where {T, T1}
+function has_edge(g::EWDiGraph{T, T1}, u::T, v::T) where {T <: Integer, T1}
   check_valid_vertices(g, u, v)
 
   ## if there is an edge u -> v, then adj(g, u) must contain v
   v ∈ map(t -> t[1], adj(g, u)) ## adj(g, u) ≡ list of tuple (vertex, weight)
 end
 
-function has_weighted_edge(g::EWDiGraph, u::T, v::T) where {T, T1}
+function has_weighted_edge(g::EWDiGraph, u::T, v::T) where {T <: Integer, T1}
   check_valid_vertices(g, u, v)
 
   find_weighted_edge(g, u, v)
 end
 
-function weight(g::EWDiGraph{T, T1}, u::T, v::T) where {T, T1}
+function weight(g::EWDiGraph{T, T1}, u::T, v::T) where {T <: Integer, T1}
   check_valid_vertices(g, u, v)
 
   if u == v
@@ -65,20 +71,20 @@ end
 ## Internal Helpers
 ##
 
-function check_valid_vertices(g::EWDiGraph{T, T1}, v₁::T, v₂::T) where {T, T1}
+function check_valid_vertices(g::EWDiGraph{T, T1}, v₁::T, v₂::T) where {T <: Integer, T1}
   n = v(g)
   for u in (v₁, v₂)
     1 ≤ u ≤ n || throw(ArgumentError("vertex $(u) not in current digraph"))
   end
 end
 
-function find_weighted_edge(g::EWDiGraph{T, T1}, u::T, v::T) where {T, T1}
+function find_weighted_edge(g::EWDiGraph{T, T1}, u::T, v::T) where {T <: Integer, T1}
   ## if there is an edge u -> v, then adj(g, u) must contain v
-  for (vₒ, w) in adj(g, u)
-    vₒ == v && return (true, w)
+  for (ix, (vₒ, w)) in enumerate(adj(g, u))
+    vₒ == v && return (true, w, ix)
   end
 
-  (false, nothing)
+  (false, nothing, nothing)
 end
 
 """
@@ -114,21 +120,21 @@ end
 
 """
 function from_file(infile::String, graph_cons, VType;
-                   WType=Int, positive_weight=true)
+                   WType=Integer, positive_weight=true)
   # VType => target type for vertices
   # WType => target type for weigth
-  g = graph_cons(0)
-  local nv = 0
+  g = graph_cons(zero(VType))
+  local nv = zero(VType)
 
   try
     open(infile, "r") do fh
       nv = 0
       for line in eachline(fh)             ## read only first line
-        (nv, ) = map(s -> parse(Int, strip(s)),
+        (nv, ) = map(s -> parse(VType, strip(s)),
                      split(line, r"\s+"))  ## expected number of vertices [opt. num. of edges)
         break
       end
-      g = graph_cons(nv)
+      g = graph_cons(VType(nv))
 
       prev_u = -1
       for line in eachline(fh)
