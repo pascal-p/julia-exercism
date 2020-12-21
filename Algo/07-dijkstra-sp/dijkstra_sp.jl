4effc5195a25167b4544fe55cd8e4820537366c3
Pascal P <lacsap_666@yahoo.fr>
2020-12-21 14:47:27 +1300

    07-dijkstra-sp move deps YA_EWD

--- Algo/07-dijkstra-sp/dijkstra_sp.jl
+++ Algo/07-dijkstra-sp/dijkstra_sp.jl
@@ -1,4 +1,4 @@
-push!(LOAD_PATH, "./lib")
+push!(LOAD_PATH, "./src")
 
 using YAG_DSP
 
--- Algo/07-dijkstra-sp/src/YAG_DSP.jl
+++ Algo/07-dijkstra-sp/src/YAG_DSP.jl
@@ -1,13 +1,14 @@
 module YAG_DSP
-  # import Base: count
 
-  # using DataStructures
+  # export AEWDiGraph
   export EWDiGraph                     # Edge Weighted Directed Graph
   export DSP                           # Dijkstra Shortest Path
 
   export add_edge, v, e, adj
   export path_to, has_path, dist_to
 
-  include("edge_weighted_di_graph.jl")
+  # include("edge_weighted_di_graph.jl")
+  include("../../18-0-ewd/src/abstract_edge_weighted_di_graph.jl")
+  include("../../18-0-ewd/src/edge_weighted_di_graph.jl")
   include("dijkstra_shortest_path.jl")
 end
