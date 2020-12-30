18fb8712978a694249b6f4e03ce05a7d61b5e212
Pascal P <lacsap_666@yahoo.fr>
2020-12-30 17:30:09 +1300

    20-1-tsp-heuristic: make it faster
    * using mutable vector instead of hash => no check then!

--- Algo/19-1-tsp/src/nn_tsp_common.jl
+++ Algo/19-1-tsp/src/nn_tsp_common.jl
@@ -19,15 +19,16 @@ end
 
   x-sorted means sorting on x portion of the coordinate
 """
-function tsp(infile::String, DT::DataType; x_sorted=false, no_index=false)
+function tsp(infile::String, DT::DataType;
+             x_sorted=false, no_index=false, algo=calc_tsp)
   vcities = no_index ? load_data(infile, DT; no_index) : load_data(infile, DT; with_index=true)
 
   tdist, visited = if !x_sorted
     sort!(vcities, by=((_ix, p)=t) -> p.x)
     hsh = Dict{Int32, Int32}([Int32(c[1]) => Int32(ix) for (ix, c) in enumerate(vcities)])
-    calc_tsp(vcities, DT, hsh)
+    calc_tsp(vcities, hsh)
   else
-    calc_tsp(vcities, DT)
+    algo(vcities)
   end
 
   (round_dist(tdist, Int), tdist, visited)
@@ -36,10 +37,12 @@ end
 
 """
 Returns min. euclidean distance to next city and the index of that city
+
+using a hash (hvisited) to keep track of cities already visited
 """
-@inline function min_dist(vcities::Vector{Tuple{Integer, Point{T}}}, DT::DataType, ix::Integer,
+@inline function min_dist(vcities::Vector{Tuple{Integer, Point{T}}}, ix::Integer,
                   hvisited::Dict{T1, Bool}, hsh::Dict{T1, T1}) where T
-  e_mdist, m_ix = typemax(DT), zero(T1)
+  e_mdist, m_ix = typemax(T), zero(T1)
 
   for c in vcities
     get(hvisited, c[1], false) && continue ## avoid cities already visited
@@ -65,20 +68,82 @@ Returns min. euclidean distance to next city and the index of that city
   (e_mdist, m_ix)
 end
 
-@inline function min_dist(vcities::Vector{Point{T}}, DT::DataType, ix::Integer, n::Integer,
-                          hvisited::Dict{T1, Bool}) where T
-  e_mdist, m_ix = typemax(DT), zero(T1)
+"""
+using vector to_visit, for remaining cities to visit (fatser than above version)
+"""
+@inline function min_dist(vcities::Vector{Tuple{Integer, Point{T}}}, ix::Integer,
+                  to_visit::Vector{T1}, hsh::Dict{T1, T1}) where T
+  e_mdist, m_ix = typemax(T), zero(T1)
 
-  for jx ∈ T1(2):n
-    get(hvisited, jx, false) && continue   ## avoid cities already visited
-
-    x_cdist = DT(vcities[ix].x - vcities[jx].x)
+  for jx ∈ to_visit
+    x_cdist = vcities[hsh[ix]][2].x - vcities[hsh[jx]][2].x
     x_cdist *= x_cdist                     ## calc. x-dist (last visited city, next city to visit)
 
     x_cdist > e_mdist && break             ## as vcities is x-coord. sorted in increasing order
     #                                      ## no need to go further when x-dist is greater than the recorded min. dist
 
-    y_cdist = DT(vcities[ix].y - vcities[jx].y)
+    y_cdist = vcities[hsh[ix]][2].y - vcities[hsh[jx]][2].y
+    e_cdist = x_cdist + y_cdist * y_cdist  ## square euclidean distance (last visited city, next city to visit)
+
+    if e_cdist < e_mdist                   ## update min. euclidean distance
+      e_mdist, m_ix = e_cdist, jx
+
+    elseif e_cdist == e_mdist              ## in case of ties, select lowest index
+      jx < m_ix && (m_ix = jx)
+    end
+  end
+
+  (e_mdist, m_ix)
+end
+
+
+"""
+for challenge
+using a hash (hvisited) to keep track of cities already visited
+"""
+@inline function min_dist(vcities::Vector{Point{T}},  ix::Integer, n::Integer,
+                          hvisited::Dict{T1, Bool}) where T
+  e_mdist, m_ix = typemax(T), zero(T1)
+
+  for jx ∈ T1(2):n
+    get(hvisited, jx, false) && continue   ## avoid cities already visited
+
+    x_cdist = vcities[ix].x - vcities[jx].x
+    x_cdist *= x_cdist                     ## calc. x-dist (last visited city, next city to visit)
+
+    x_cdist > e_mdist && break             ## as vcities is x-coord. sorted in increasing order
+    #                                      ## no need to go further when x-dist is greater than the recorded min. dist
+
+    y_cdist = vcities[ix].y - vcities[jx].y
+    e_cdist = x_cdist + y_cdist * y_cdist  ## square euclidean distance (last visited city, next city to visit)
+
+    if e_cdist < e_mdist                   ## update min. euclidean distance
+      e_mdist, m_ix = e_cdist, jx
+
+    elseif e_cdist == e_mdist              ## in case of ties, select lowest index
+      jx < m_ix && (m_ix = jx)
+    end
+  end
+
+  (e_mdist, m_ix)
+end
+
+"""
+for challenge
+using a hash (hvisited) to keep track of cities already visited
+"""
+@inline function min_dist(vcities::Vector{Point{T}},  ix::Integer, n::Integer,  # DT::DataType,
+                          to_visit::Vector{T1}) where T
+  e_mdist, m_ix = typemax(T), zero(T1)
+
+  for jx ∈ to_visit
+    x_cdist = vcities[ix].x - vcities[jx].x
+    x_cdist *= x_cdist                     ## calc. x-dist (last visited city, next city to visit)
+
+    x_cdist > e_mdist && break             ## as vcities is x-coord. sorted in increasing order
+    #                                      ## no need to go further when x-dist is greater than the recorded min. dist
+
+    y_cdist = vcities[ix].y - vcities[jx].y
     e_cdist = x_cdist + y_cdist * y_cdist  ## square euclidean distance (last visited city, next city to visit)
 
     if e_cdist < e_mdist                   ## update min. euclidean distance
--- Algo/20-1-tsp-heuristic/nn_tsp.jl
+++ Algo/20-1-tsp-heuristic/nn_tsp.jl
@@ -12,18 +12,21 @@ include("./src/common.jl")
 include("./src/nn_tsp_common.jl")
 
 
-function calc_tsp(vcities::Vector{Tuple{Integer, Point{T}}}, DT::DataType,
-                  hsh::Dict{T1, T1}) where T
+"""
+
+using mutable vector (to_visit) for the cities to visit
+"""
+function calc_tsp(vcities::Vector{Tuple{Integer, Point{T}}}, hsh::Dict{T1, T1}) where T
   n = T1(length(vcities))
-  visited, hvisited = Vector{T1}(undef, n), Dict{T1, Bool}()
+  visited = Vector{T1}(undef, n)
   visited[1] = one(T1)
-  hvisited[one(T1)] = true
-  ix, tot_dist = one(T1), zero(DT)
+  ix, tot_dist = one(T1), zero(T)
+  to_visit::Vector{T1} = map(((jx, _p)=t) -> jx, vcities) |> a -> filter(ix -> ix > 1, a)
 
   for kx in 2:n
-    (e_mdist, m_ix) = min_dist(vcities, DT, ix, hvisited, hsh)
+    (e_mdist, m_ix) = min_dist(vcities, ix, to_visit, hsh)
     visited[kx] = ix = m_ix
-    hvisited[m_ix] = true
+    deleteat!(to_visit, findfirst(idx -> idx == m_ix, to_visit))
     tot_dist += √(e_mdist)
   end
 
@@ -37,18 +40,46 @@ end
 This version is for the challenge which uses a specification file nn.txt which is
 pre x-sorted already
 
-visited is preallocated
+visited is pre-allocated
+using a hash (hvisited) to keep track of city indexes already visited
 """
-function calc_tsp(vcities::Vector{Point{T}}, DT::DataType) where T
+function calc_tsp(vcities::Vector{Point{T}}) where T
   n = T1(length(vcities))
   visited, hvisited = Vector{T1}(undef, n), Dict{T1, Bool}()
-  ix, tot_dist = one(T1), zero(DT)
+  ix, tot_dist = one(T1), zero(T)
 
   for kx in 2:n
-    (e_mdist, m_ix) = min_dist(vcities, DT, ix, n, hvisited)
+    (e_mdist, m_ix) = min_dist(vcities, ix, n, hvisited)
     visited[kx] = ix = m_ix
     hvisited[m_ix] = true
-    tot_dist += DT(√(e_mdist))
+    tot_dist += √(e_mdist)
+  end
+
+  tot_dist += euclidean_dist(vcities[visited[end]], vcities[one(T1)])
+  return (tot_dist, visited)
+end
+
+"""
+This version is for the challenge which uses a specification file nn.txt which is
+pre x-sorted already
+
+visited is pre-allocated
+
+instead of using a hash (hvisited) to keep track of city indexes already visited, use
+a vector where we delete the city indexes already visited (this turns out to be significantly
+faster)
+"""
+function calc_tsp_f(vcities::Vector{Point{T}}) where T
+  n = T1(length(vcities))
+  visited = Vector{T1}(undef, n)
+  ix, tot_dist = one(T1), zero(T)
+  to_visit::Vector{T1} = collect(T1(2):n)
+
+  for kx in 2:n
+    (e_mdist, m_ix) = min_dist(vcities, ix, n, to_visit)
+    visited[kx] = ix = m_ix
+    deleteat!(to_visit, findfirst(idx -> idx == m_ix, to_visit))
+    tot_dist += √(e_mdist)
   end
 
   tot_dist += euclidean_dist(vcities[visited[end]], vcities[one(T1)])
