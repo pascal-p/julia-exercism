0019645fa0b8e585cdce5578966bccd9a2c00281
Pascal P <lacsap_666@yahoo.fr>
2021-05-22 13:13:25 +1200

    more tests

--- robot-simulator/runtests.jl
+++ robot-simulator/runtests.jl
@@ -3,59 +3,68 @@ using Test
 include("robot-simulator.jl")
 
 @testset "constructor" begin
-    r = Robot((0, 0), NORTH)
-    @test position(r) == Point(0, 0)
-    @test heading(r) == NORTH
+  r = Robot((0, 0), NORTH)
+  @test position(r) == Point(0, 0)
+  @test heading(r) == NORTH
 
-    r = Robot((-1, -1), SOUTH)
-    @test position(r) == Point{Int}(-1, -1)
-    @test heading(r) == SOUTH
+  r = Robot((-1, -1), SOUTH)
+  @test position(r) == Point{Int}(-1, -1)
+  @test heading(r) == SOUTH
 end
 
 @testset "mutating functions should return robot" begin
-    r = Robot((0, 0), NORTH)
-    @test r == turn_right!(r)
-    @test r == turn_left!(r)
-    @test r == advance!(r)
-    @test r == move!(r, "A")
+  r = Robot((0, 0), NORTH)
+  @test r == turn_right!(r)
+  @test r == turn_left!(r)
+  @test r == advance!(r)
+  @test r == move!(r, "A")
 end
 
-# @testset "rotate +π/2" begin
-#     r = Robot((0, 0), NORTH)
-#     turn_right!(r)
-#     @test position(r) == Point(0, 0)
-#     @test heading(r) == EAST
-#     turn_right!(r)
-#     @test heading(r) == SOUTH
-#     turn_right!(r)
-#     @test heading(r) == WEST
-#     turn_right!(r)
-#     @test heading(r) == NORTH
-# end
+@testset "rotate +π/2" begin
+  r = Robot((0, 0), NORTH)
 
-# @testset "rotate -π/2" begin
-#     r = Robot((0, 0), NORTH)
-#     turn_left!(r)
-#     @test position(r) == Point(0, 0)
-#     @test heading(r) == WEST
-#     turn_left!(r)
-#     @test heading(r) == SOUTH
-#     turn_left!(r)
-#     @test heading(r) == EAST
-#     turn_left!(r)
-#     @test heading(r) == NORTH
-# end
+  turn_right!(r)
+  @test position(r) == Point(0, 0)
+  @test heading(r) == EAST
 
-# @testset "advance" begin
-#     r = Robot((0, 0), NORTH)
-#     advance!(r)
-#     @test heading(r) == NORTH
-#     @test position(r) == Point(0, 1)
+  turn_right!(r)
+  @test heading(r) == SOUTH
 
-#     @test position(advance!(Robot((0, 0), SOUTH))) == Point(0, -1)
-#     @test position(advance!(Robot((0, 0), EAST))) == Point(1, 0)
-#     @test position(advance!(Robot((0, 0), WEST))) == Point(-1, 0)
-# end
+  turn_right!(r)
+  @test heading(r) == WEST
+
+  turn_right!(r)
+  @test heading(r) == NORTH
+end
+
+@testset "rotate -π/2" begin
+  r = Robot((0, 0), NORTH)
+
+  turn_left!(r)
+  @test position(r) == Point(0, 0)
+  @test heading(r) == WEST
+
+  turn_left!(r)
+  @test heading(r) == SOUTH
+
+  turn_left!(r)
+  @test heading(r) == EAST
+
+  turn_left!(r)
+  @test heading(r) == NORTH
+end
+
+@testset "advance" begin
+  r = Robot((0, 0), NORTH)
+  advance!(r)
+
+  @test heading(r) == NORTH
+  @test position(r) == Point(0, 1)
+
+  @test position(advance!(Robot((0, 0), SOUTH))) == Point(0, -1)
+  @test position(advance!(Robot((0, 0), EAST))) == Point(1, 0)
+  @test position(advance!(Robot((0, 0), WEST))) == Point(-1, 0)
+end
 
 # @testset "instructions" begin
 #     @testset "moving east and north from README" begin
