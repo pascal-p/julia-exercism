264895156c7857cbb3a6191fddc0585fc3746aed
Pascal P <lacsap_666@yahoo.fr>
2022-01-29 20:37:03 +1300

    rna: update

--- rna-transcription/rna-transcription.jl
+++ rna-transcription/rna-transcription.jl
@@ -9,15 +9,10 @@ const MAP_DNA_RNA = Dict{Char, Char}(d => r for (d, r) in zip(DNA_Nucleotides, R
 
 function to_rna(dna::String)
   isempty(dna) && return ""
-  
   rna::String = ""
-  dna = uppercase(dna)  
-  for ch in dna
+  for ch ∈ uppercase(dna)
     ch ∉ DNA_Nucleotides && throw(ErrorException("$(ch) ∉ $(DNA_Nucleotides)"))
-  
     rna = string(rna, MAP_DNA_RNA[ch])
   end
-  
   rna
 end
-
