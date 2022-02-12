b4618be36aa2a14d67b49bc740c804697326e4d5
Pascal P <lacsap_666@yahoo.fr>
2022-02-12 22:51:21 +1300

    bowling: complete

--- bowling/bowling.jl
+++ bowling/bowling.jl
@@ -5,6 +5,8 @@ const MAX_PINS_VALUE = 10
 
 NInt = Union{<: Integer, Nothing}
 PairNInt = Pair{NInt, NInt}
+SType = UInt16  # Score Type
+ZERO = zero(UInt16)
 
 mutable struct BowlingGame
   pins::Vector{PairNInt}
@@ -12,7 +14,7 @@ mutable struct BowlingGame
   ix::Integer
 
   function BowlingGame()
-    pins = Vector{PairNInt}(undef, NUM_FRAMES + 1)
+    pins = Vector{PairNInt}(undef, NUM_FRAMES + 2)
     pair = PairNInt(nothing, nothing)
     new(pins, pair, 0)
   end
@@ -31,15 +33,15 @@ function update_pins!(game::BowlingGame)
   game.pair = nothing => nothing
 end
 
-function sum(pair::PairNInt)::Integer
+function sum(pair::PairNInt)::SType
   if isnothing(pair.first) && isnothing(pair.second)
-    zero(Integer)
+    ZERO
   elseif isnothing(pair.second)
     pair.first
   elseif isnothing(pair.first)
     pair.second
   else
-    sum(pair)
+    pair.first + pair.second
   end
 end
 
@@ -47,19 +49,19 @@ end
 """
   Record a roll for the current game
 """
-function roll!(game::BowlingGame, pins::Integer)
-  (pins < 0 || pins > MAX_PINS_VALUE) &&
+function roll!(game::BowlingGame, pins::SType)
+  pins > MAX_PINS_VALUE &&
     throw(ArgumentError("pins cannot be more than $(MAX_PINS_VALUE) or less than 0"))
-  #
-  if length(game.pins) == NUM_FRAMES
-    # extra-rolls are alowed provided that last frame was a strike or a spare
-    if length(game.pair) == 1
+
+  if game.ix == NUM_FRAMES
+    # extra-rolls are allowed provided that last frame was a strike or a spare
+    if !isnothing(game.pair.first) && isnothing(game.pair.second)
       # in case of spare we are allowed 1 bonus roll, not 2
-      isspare(game.pins[end]) && throw("Cannot roll more")
-      pins == MAX_PINS_VALUE && throw("Current pins cannot be a strike")
+      isspare(game.pins[game.ix]) && throw(ArgumentError("Cannot roll more")) # because now we have a full pair which means 2 rolls
+      pins == MAX_PINS_VALUE && throw(ArgumentError("Current pins cannot be a strike"))
     else
       # game.pair is empty && last frame was not a spare nor a strike
-      sum(game.pins[end]) == 0 && throw("Cannot roll more")
+      sum(game.pins[game.ix]) == ZERO && throw(ArgumentError("Cannot roll more"))
     end
   end
 
@@ -69,13 +71,13 @@ function roll!(game::BowlingGame, pins::Integer)
 
   # case of a strike
   if pins == MAX_PINS_VALUE && isnothing(game.pair.first)
-    game.pair = pins => 0
+    game.pair = pins => ZERO
     update_pins!(game)
   else
     if isnothing(game.pair.first)
       game.pair = pins => nothing
     elseif isnothing(game.pair.second)
-      sum(game.pair.first => pins) > MAX_PINS_VALUE &&
+      game.pair.first + pins > MAX_PINS_VALUE &&
         throw(ArgumentError("frame cannot be > $(MAX_PINS_VALUE)"))
       game.pair = game.pair.first => pins
       update_pins!(game)
@@ -83,4 +85,81 @@ function roll!(game::BowlingGame, pins::Integer)
       throw(ArgumentError("pair if of length % 2"))
     end
   end
+end
+
+function roll!(game::BowlingGame, pins::Int)
+  pins < 0 && throw(ArgumentError("pins cannot be less than 0"))
+  roll!(game, SType(pins))
+end
+
+function score(game::BowlingGame)::SType
+  # is there a last (unfinished) pair
+  if !isnothing(game.pair.first) && isnothing(game.pair.second)
+    game.pair = game.pair.first => ZERO
+    update_pins!(game)
+  end
+
+  npins, score, n = game.pins, ZERO, game.ix
+
+  (n == NUM_FRAMES + 1) && isstrike(npins[game.ix - 1]) && isstrike(npins[game.ix]) &&
+      throw(ArgumentError("Cannot determine score as 10tn and 11th frame were strike")) # would need one more frame
+
+  if n == NUM_FRAMES
+    # if strike or spare we need bonus roll(s)
+    sum(npins[n]) == MAX_PINS_VALUE && throw(ArgumentError("Missing bonus rolls"))
+    score = calc_score(npins, n)
+
+  elseif n > NUM_FRAMES
+    sum(npins[n - 1]) != MAX_PINS_VALUE &&
+      throw(ArgumentError("Bonus roll(s) are not allowed unless last frame was spare or strike"))
+
+    score = calc_score(npins, n)
+  else
+    throw(ArgumentError("Less than $(NUM_FRAMES) frames"))
+  end
+
+  score
+end
+
+"""
+   calc_score(npins::Vector{PairNInt})::UInt
+
+Calculate the score given the vector of rolls (or pins)
+
+using a stack to keep track of previous states, most notably whether a
+strike or a spare was recorded in a previous frame.
+"""
+function calc_score(npins::Vector{PairNInt}, n)::SType
+  score, cval = fill(zero(SType), length(npins)), ZERO
+  stack = fill("", length(npins) + 1)
+  stack[1] = "sentinelle"
+
+  for (ix, p) ∈ enumerate(npins[1:n])
+    if iszero(sum(p))
+      stack[ix + 1] = "open"
+      continue # score is already set to 0
+    end
+
+    cval = sum(p)
+    if cval < MAX_PINS_VALUE
+      stack[ix + 1] = "open"
+    elseif isstrike(p)
+      stack[ix + 1] = "strike"
+    else
+      stack[ix + 1] = "spare"
+    end
+
+    if stack[ix] == "spare"
+      score[ix - 1] += p.first # update prev. score
+    elseif stack[ix] == "strike"
+      # update prev. score
+      score[ix - 1] += p.first != MAX_PINS_VALUE ? cval : p.first
+      # update prev. prev. score
+      ix ≥ 3 && stack[ix - 1] == "strike" && (score[ix - 2] += p.first)
+    end
+
+    score[ix] = cval
+  end
+
+  sum(score[1:NUM_FRAMES])
 end
--- bowling/runtests.jl
+++ bowling/runtests.jl
@@ -2,40 +2,167 @@ using Test
 
 include("bowling.jl")
 
-function roll_new_game!(game::BowlingGame, rolls::Vector{<: Integer})
+function roll_new_game(rolls::Vector{<: Integer})
   # roll!.(game, rolls)
+  game = BowlingGame()
   for roll ∈ rolls
     roll!(game, roll)
   end
   game
 end
 
+function roll_new_game(rolls::Vector)
+  @assert length(rolls) == 0
+  BowlingGame()
+end
+
 @testset "roll" begin
   @testset "two rolls in a frame cannot score > 10 points" begin
-    game = BowlingGame()
-    roll_new_game!(game, [5])
-
+    game = roll_new_game([5])
     @test_throws ArgumentError roll!(game, 6)
   end
 
   @testset "the second bonus rolls after a strike in the last frame cannot be a strike if the first one is not a strike" begin
-    game = BowlingGame()
-    roll_new_game!(game, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 6])
-
+    game = roll_new_game([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 6])
     @test_throws ArgumentError roll!(game, 10)
   end
 
   @testset "test second bonus roll after a strike in the last frame cannot score more than 10 points" begin
-    game = BowlingGame()
-    roll_new_game!(game, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10])
-
+    game = roll_new_game([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10])
     @test_throws ArgumentError roll!(game, 11)
   end
 
   @testset "test two bonus rolls after a strike in the last frame cannot score more than 10 points" begin
-    game = BowlingGame()
-    roll_new_game!(game, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 5])
+    game = roll_new_game([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 5])
+    @test_throws ArgumentError roll!(game, 6)
+  end
 
-     @test_throws ArgumentError roll!(game, 6)
+  @testset "cannot roll if game already has ten frames" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    game = roll_new_game(rolls)
+    @test_throws ArgumentError roll!(game, 0)
+  end
+
+  @testset "cannot roll after bonus roll for spare" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 2]
+    game = roll_new_game(rolls)
+    @test_throws ArgumentError roll!(game, 2)
+  end
+
+  @testset "cannot_roll_after_bonus_rolls_for_strike(" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 3, 2]
+    game = roll_new_game(rolls)
+    @test_throws ArgumentError roll!(game, 2)
+  end
+end
+
+@testset "score" begin
+  @testset "should be able to score a game with all zeros" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    game = roll_new_game(rolls)
+    @test score(game) == zero(SType)
+  end
+
+  @testset "should be able to score a game with no strikes or spares" begin
+    rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
+    game = roll_new_game(rolls)
+    @test score(game) == SType(90)
+  end
+
+  @testset "points scored in the roll after a spare are counted twice" begin
+    rolls = [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    game = roll_new_game(rolls)
+    @test score(game) == SType(16)
+  end
+
+  @testset "test consecutive spares each get a one roll bonus" begin
+    rolls = [5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    game = roll_new_game(rolls)
+    #println(game)
+    @test score(game) == SType(31)
+  end
+
+  @testset "a spare in the last frame gets a one roll bonus that is counted once" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 7]
+    game = roll_new_game(rolls)
+    @test score(game) == SType(17)
+  end
+
+  @testset "points scored in the two rolls after a strike are counted twice as a bonus" begin
+    rolls = [10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    game = roll_new_game(rolls)
+    @test score(game) == SType(26)
+  end
+
+  @testset "points scored in the two rolls after a strike are counted twice as a bonus" begin
+    rolls = [10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    game = roll_new_game(rolls)
+    @test score(game) == SType(81)
+  end
+
+  @testset "a strike in the last frame gets a two roll bonus that is counted once" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 1]
+    game = roll_new_game(rolls)
+    @test score(game) == SType(18)
+   end
+
+  @testset "rolling a spare with the two roll bonus does not get a bonus roll" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 3]
+    game = roll_new_game(rolls)
+    @test score(game) == SType(20)
+  end
+
+  @testset "a strike with the one roll bonus after a spare in the last frame does not get a bonus" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10]
+    game = roll_new_game(rolls)
+    @test score(game) == SType(20)
+  end
+
+  @testset "strikes_with_the_two_roll_bonus_do_not_get_bonus_roll" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]
+    game = roll_new_game(rolls)
+    @test score(game) == SType(30)
+  end
+
+  @testset "all strikes is a perfect game" begin
+    rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+    game = roll_new_game(rolls)
+    @test score(game) == SType(300)
+  end
+
+  @testset "" begin
+  end
+end
+
+
+@testset "Exception" begin
+  @testset "an unstarted game cannot be scored" begin
+    rolls = []
+    game = roll_new_game(rolls)
+    @test_throws ArgumentError score(game)
+  end
+
+  @testset "an incomplete game cannot be scored" begin
+    rolls = [0, 0]
+    game = roll_new_game(rolls)
+    @test_throws ArgumentError score(game)
+  end
+
+  @testset "bonus rolls for a strike in the last frame must be rolled before score can be calculated" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10]
+    game = roll_new_game(rolls)
+    @test_throws ArgumentError score(game)
+  end
+
+  @testset "both bonus rolls for a strike in the last frame must be rolled before score can be calculated" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10] # missing fill
+    game = roll_new_game(rolls)
+    @test_throws ArgumentError score(game)
+  end
+
+  @testset "bonus roll for a spare in the last frame must be rolled before score can be calculated" begin
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3]
+    game = roll_new_game(rolls)
+    @test_throws ArgumentError score(game)
   end
 end
