66619d6ff16033c5f8e7261925d9a331ca48e856
Pascal P <lacsap_666@yahoo.fr>
2022-02-21 22:38:26 +1300

    go_counting: part 1

--- go_counting/go_counting.jl
+++ go_counting/go_counting.jl
@@ -1,26 +1,99 @@
-const BLACK = :B
-const WHITE = :W
-const NONE = :_
+const BLACK = 'B'
+const WHITE = 'W'
+const NONE = ' '
+
+TT = UInt8
 
 struct Point
-  x::UInt8
-  y::UInt::8
+  x::TT
+  y::TT
 end
+
+SP = Set{Union{Point, Nothing}}
+VP = Vector{Union{Point, Nothing}}
 
 struct GoBoard
   board::Vector{String}
-  x_lim::UInt8
-  y_lim::UInt8
+  x_lim::TT
+  y_lim::TT
+
+  function GoBoard(board::Vector{String})
+    @assert length(board) > 0
+    y_lim = length(board)
+    x_lim = length(board[1])
+    c_board = [board...]
+    new(c_board, x_lim, y_lim)
+  end
 end
 
 ## Public API
 
-function territory(goboard::GoBoard, x::UInt, y::UInt)::Tuple{String, Set{Point}}
+function territory(goboard::GoBoard, x::TT, y::TT)::Tuple{Char, SP}
+  (!(1 ≤ x ≤ goboard.x_lim) || !(1 ≤ y ≤ goboard.y_lim)) && throw(ArgumentError("Invalid coordinate"))
+
+  goboard.board[y][x] != NONE && return (NONE, Set())
+
+  neighbors = get_neighbors(goboard, x, y)
+  length(neighbors) == 0 && return (NONE, Set([Point(x, y)])) # very unlikely, but for board with 1 cell
+
+  count = get_color_partition(goboard, neighbors)
+  count[BLACK] > 0 && count[WHITE] == 0 && count[NONE] == 0 && (return (BLACK, Set([Point(x, y)])))
+  count[BLACK] == 0 && count[WHITE] > 0 && count[NONE] == 0 && (return (WHITE, Set([Point(x, y)])))
+
+  get_territory(goboard, neighbors)
+end
+
+function territory(goboard::GoBoard, x::Integer, y::Integer)
+  (x < 1 || y < 1) && throw(ArgumentError("Invalid coordinate"))
+  territory(goboard, TT(x), TT(y)) # truncation can happen!
 end
 
 function territories(goboard::GoBoard)::Dict{String, Set{Point}}
+  # TBD
 end
 
 ## Private Helpers
 
-## TBD
+function get_neighbors(goboard::GoBoard, x::TT, y::TT)::Vector{Union{Point, Nothing}}
+  [Point(x - 1, y), Point(x + 1, y), Point(x, y - 1), Point(x, y + 1)] |>
+    l -> filter((p::Point) -> (1 ≤ p.x ≤ goboard.x_lim) && (1 ≤ p.y ≤ goboard.y_lim), l)
+end
+
+function get_color_partition(goboard::GoBoard, neighbors::VP)::Dict{Char, TT}
+  count = Dict{Char, TT}(BLACK => 0, NONE => 0, WHITE => 0)
+  for p ∈ neighbors
+    count[goboard.board[p.y][p.x]] += 1
+  end
+  count
+end
+
+function get_territory(goboard::GoBoard, neighbors::VP)::Tuple{Char, SP}
+  done, territory = VP(), VP()
+  color = ' '
+
+  while length(neighbors) > 0
+    p = neighbors[1]
+    xy_color = goboard.board[p.y][p.x]
+    if color == ' '
+      (xy_color) != NONE && (color = xy_color)
+    elseif color == WHITE && xy_color == BLACK
+      color = NONE
+    elseif color == WHITE && xy_color == BLACK
+      color = NONE
+    end
+    if xy_color ∈ (BLACK, WHITE)
+      neighbors = [neighbors[2:end]...]
+      push!(done, p)
+    else
+      c_neighbors = [
+        ρ for ρ ∈ get_neighbors(goboard, p.x, p.y) if ρ ∉ neighbors && p ∉ done
+      ]
+      push!(territory, p)
+      push!(done, p)
+      neighbors = [neighbors[2:end]..., c_neighbors...]
+    end
+  end
+
+  color == ' ' && (color = NONE)
+  (color, Set(territory))
+end
--- go_counting/runtests.jl
+++ go_counting/runtests.jl
@@ -3,7 +3,27 @@ using Test
 include("go_counting.jl")
 
 @testset "territory" begin
+  board = GoBoard(["  B  ", " B B ", "B W B", " W W ", "  W  "])
+
+  @test territory(board, 3, 1) == (NONE, Set())
+  @test territory(board, 3, 4) == (WHITE, Set([Point(3, 4)]))
+  @test territory(board, 1, 2) == (BLACK, Set([Point(1, 1), Point(1, 2), Point(2, 1)]))
+  @test territory(board, 3, 4) == (WHITE, Set([Point(3, 4)]))
+  @test territory(board, 2, 5) == (NONE, Set([Point(1, 4), Point(1, 5), Point(2, 5)]))
+  @test territory(board, 5, 5) == (NONE, Set([Point(5, 4), Point(4, 5), Point(5, 5)]))
+  @test territory(board, 3, 3) == (NONE, Set())
 end
 
+@testset "territory exception" begin
+  board = GoBoard(["  B  ", " B B ", "B W B", " W W ", "  W  "])
+
+  @test_throws ArgumentError territory(board, -1, 1)
+  @test_throws ArgumentError territory(board, 6, 1)
+  @test_throws ArgumentError territory(board, 0, -1)
+  @test_throws ArgumentError territory(board, 1, 10)
+end
+
+
 @testset "territories" begin
+  # TBD
 end
