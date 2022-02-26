b0f09520ad41e4ae9041712d3a04e7d5c0b2c27f
Pascal P <lacsap_666@yahoo.fr>
2022-02-26 22:24:51 +1300

    domino: complete

--- dominoes/dominoes.jl
+++ dominoes/dominoes.jl
@@ -1,4 +1,3 @@
-
 TT = UInt8
 Domino = Tuple{TT, TT}
 Dominoes = Vector{Domino}
@@ -12,8 +11,10 @@ function can_chain(dominoes::Dominoes)::Union{Nothing, Dominoes}
   end
 
   n = length(dominoes)
+  cp_dominoes = [dominoes...]
   for domino ∈ dominoes
-    res, chain = find_chain(filter(x -> x != domino, dominoes), [domino], TT(n), TT(1))
+    remove!(cp_dominoes::Dominoes, domino::Domino)
+    res, chain = find_chain(cp_dominoes, [domino], TT(n), TT(1))
     res && return chain
   end
 
@@ -25,7 +26,48 @@ can_chain(dominoes::Vector{Any}) = length(dominoes) == 0 ? [] : nothing
 function find_chain(dominoes::Dominoes, chain::Dominoes, n::TT, p::TT)::Tuple{Bool, Dominoes}
   if length(dominoes) == 0 && length(chain) == n
     return chain[1][1] == chain[end][2] ? (true, chain) : (false, [])
+  elseif length(dominoes) == 0
+    return (false, [])
   end
 
-  # TBD ...
+  curr = chain[end]
+  candidates = find_next(dominoes, curr)
+  if length(candidates) == 0
+    # no more candidate, are we at the end of a valid chain?
+    if chain[1][1] == dominoes[1][2] && chain[end][2] == dominoes[1][1]
+      push!(chain, dominoes[end])
+      return length(chain) == n ? (true, chain) : (false, [])
+    end
+    return (false, [])
+  end
+
+  while length(candidates) > 0
+    cand = candidates[1]
+    remove!(dominoes, cand)
+    candidates = candidates[2:end]
+    res, fchain = find_chain(dominoes, [chain..., cand], n, p + TT(1))
+    res && return (res, fchain)
+    # backtrack
+    push!(dominoes, cand)
+  end
+
+  return (false, [])
+end
+
+function remove!(dominoes::Dominoes, domino::Domino)
+  ix = findfirst(d -> d == domino || (d[1] == domino[2] && d[2] == domino[1]),
+                 dominoes)
+  ix != nothing && deleteat!(dominoes, ix)
+end
+
+function find_next(dominoes::Dominoes, domino::Domino)::Union{Nothing, Dominoes}
+  s = Set{Domino}(
+    d for d ∈ dominoes if domino[2] == d[1]
+  )
+  if length(s) == 0
+    s = Set{Domino}(
+      (d[2], d[1]) for d ∈ dominoes if domino[2] == d[2]
+    )
+  end
+  Dominoes([s...])
 end
--- dominoes/runtests.jl
+++ dominoes/runtests.jl
@@ -12,7 +12,49 @@ include("dominoes.jl")
     @test can_chain([(TT(1), TT(1))]) == [(TT(1), TT(1))]
   end
 
-   @testset "singleton that cannot be chainned " begin
-     @test can_chain([(TT(1), TT(2))]) == nothing
+  @testset "singleton that cannot be chainned" begin
+    @test isnothing(can_chain([(TT(1), TT(2))]))
+  end
+
+  @testset "2 element list" begin
+    @test can_chain([(TT(1), TT(2)), (TT(1), TT(2))]) == [(TT(1), TT(2)), (TT(2), TT(1))]
+  end
+
+  @testset "2 element list that cannot be chainned" begin
+    @test isnothing(can_chain([(TT(1), TT(2)), (TT(1), TT(3))]))
+  end
+
+  @testset "3 element list" begin
+    @test can_chain([(TT(1), TT(2)), (TT(3), TT(1)), (TT(2), TT(3))]) == [(TT(1), TT(2)), (TT(2), TT(3)), (TT(3), TT(1))]
+  end
+
+  @testset "can reverse" begin
+    @test can_chain([(TT(1), TT(2)), (TT(1), TT(3)), (TT(2), TT(3))]) == [(TT(1), TT(2)), (TT(2), TT(3)), (TT(3), TT(1))]
+  end
+
+  @testset "3 element list that cannot be chainned" begin
+    @test isnothing(can_chain([(TT(1), TT(2)), (TT(4), TT(1)), (TT(2), TT(3))]))
+  end
+
+  @testset "test disconnected single isolated" begin
+    @test isnothing(can_chain([(TT(1), TT(2)), (TT(2), TT(3)), (TT(3), TT(1)), (TT(4), TT(4))]))
+  end
+
+  @testset "test need backtrack" begin
+    @test can_chain([(TT(1), TT(2)), (TT(2), TT(3)), (TT(3), TT(1)), (TT(2), TT(4)), (TT(2), TT(4))]) ==
+      [(TT(1), TT(2)), (TT(2), TT(4)), (TT(4), TT(2)), (TT(2), TT(3)), (TT(3), TT(1))]
+  end
+
+  @testset "test separate loops" begin
+    @test can_chain([(TT(1), TT(2)), (TT(2), TT(3)), (TT(3), TT(1)), (TT(1), TT(1)), (TT(2), TT(2)), (TT(3), TT(3))]) ==
+      [(0x01, 0x02), (0x02, 0x02), (0x02, 0x03), (0x03, 0x03), (0x03, 0x01), (0x01, 0x01)]
+  end
+
+  @testset "9 elements" begin
+    @test can_chain([(TT(1), TT(2)), (TT(5), TT(3)), (TT(3), TT(1)), (TT(1), TT(2)),
+                     (TT(2), TT(4)), (TT(1), TT(6)), (TT(2), TT(3)), (TT(3), TT(4)),
+                     (TT(5), TT(6))]) ==
+      [(0x01, 0x02), (0x02, 0x04), (0x04, 0x03), (0x03, 0x01), (0x01, 0x02), (0x02, 0x03),
+       (0x03, 0x05), (0x05, 0x06), (0x06, 0x01)]
   end
 end
