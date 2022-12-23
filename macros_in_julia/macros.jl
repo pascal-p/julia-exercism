5dd25da1cbc60b48a36ff21127916e3b5d202f75
Pascal P <lacsap_666@yahoo.fr>
2022-12-24 12:02:31 +1300

    macros in julia/4

--- macros_in_julia/macros.jl
+++ macros_in_julia/macros.jl
@@ -381,6 +381,51 @@ end
 # ╔═╡ 44268771-432b-42be-96a1-7ac08a8e74ca
 typeof(:Nothing), typeof(Nothing)
 
+# ╔═╡ f3c148ff-22d3-4bdb-a7bc-c02e16bef3ee
+md"""
+### Example 5: Checking type and limits 
+"""
+
+# ╔═╡ 15bc7d93-5b81-4baa-b4ca-ecac918e814f
+extract_arg(arg) = typeof(arg[2]) == Symbol ? arg[2] : arg[2].args[1]
+
+# ╔═╡ c736370c-39e3-46a2-8d33-c5ec7402c154
+#
+# convenience macro whose aim is to inject the following 'guard' condition:
+#   ($x ≤ zero(typeof($x)) || $x > LIM) && throw(DomainError("Should be > 0 and ≤ $(LIM)"))
+# into the target function body
+macro check_int_arg(fn::Expr)
+  local x = extract_arg(fn.args[1].args)
+  # replace body
+  fn.args[2] = quote
+    ($x ≤ zero(typeof($x)) || $x > LIM) && throw(DomainError("Should be > 0 and ≤ $(LIM)"))
+    $(fn.args[2])
+  end
+  fn
+end
+
+# ╔═╡ c1388360-4a21-49ea-9f31-f6f05c282fc0
+#
+# convenience macro whose aim is to inject the following 'guard' condition:
+#   $(x) > typeof($(x))(LIM) && throw(DomainError("Should be > 0 and ≤ $(LIM)"))
+# into the target function body
+#
+macro check_uint_arg(fn::Expr)
+  local x = extract_arg(fn.args[1].args)
+  # replace body
+  fn.args[2] = quote
+    $(x) > typeof($(x))(LIM) && throw(DomainError("Should be > 0 and ≤ $(LIM)"))
+    $(fn.args[2])
+  end
+  fn
+end
+
+# ╔═╡ 35384000-dbe1-4b18-b616-2ce0493ff6b2
+@check_uint_arg on_square(square::Unsigned) = (typeof(square))(2) ^ (square - one(typeof(square)))
+
+# ╔═╡ 17407d62-d960-47c4-b034-6aac3f612ad8
+@check_int_arg on_square(square::Integer) = on_square(Unsigned(square))
+
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
@@ -689,5 +734,11 @@ version = "17.4.0+0"
 # ╠═b6dc3770-1fc4-4740-aa36-dd01f386c3e0
 # ╠═a5d7558b-1d48-4c17-8e9e-0c3708bcf28f
 # ╠═44268771-432b-42be-96a1-7ac08a8e74ca
+# ╟─f3c148ff-22d3-4bdb-a7bc-c02e16bef3ee
+# ╠═15bc7d93-5b81-4baa-b4ca-ecac918e814f
+# ╠═c736370c-39e3-46a2-8d33-c5ec7402c154
+# ╠═c1388360-4a21-49ea-9f31-f6f05c282fc0
+# ╠═35384000-dbe1-4b18-b616-2ce0493ff6b2
+# ╠═17407d62-d960-47c4-b034-6aac3f612ad8
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
