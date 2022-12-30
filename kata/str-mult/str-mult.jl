685eb32581be832126ea65bdb77b14740d6ed925
Pascal P <lacsap_666@yahoo.fr>
2022-12-31 10:25:13 +1300

    str-mult - iter 6 - mult with vectorization

--- kata/str-mult/str-mult.jl
+++ kata/str-mult/str-mult.jl
@@ -1,22 +1,5 @@
 const T = Int16
 
-# #
-# # Using BigInt makes it too easy (and why using string then?)
-# #
-# function mul_cheat(s₁::String, s₂::String)::String
-#   r = 0
-#   s₁, s₂ = s₁ < s₂ ? (s₁, s₂) : (s₂, s₁)
-#   xs = split(s₁, "") |> v -> parse.(BigInt, v)
-#   y = parse(BigInt, s₂)
-#   p = 0
-#   for x ∈ xs |> reverse
-#     sm  = string(y * x, "0"^p)
-#     r += parse(BigInt, sm)
-#     p += 1
-#   end
-#   string(r)
-# end
-
 #
 # String only version
 #
@@ -34,7 +17,9 @@ function mul(s₁::String, s₂::String)::String
   xs = split(s₁, "") |> reverse |> v -> parse.(T, v)
   ys = split(s₂, "") |> reverse |> v -> parse.(T, v)
   vv = mul(xs, ys)
+  # println("before vv: <$(vv)>")
   alignseq!(vv)
+  # println(" after vv: <$(vv)>")
   s = sumseq(vv) |> reverse |> v -> join(v, "") # sum && stringify
 
   postprocessing(s, pos₁, pos₂, sign₁, sign₂)
@@ -43,19 +28,8 @@ end
 function mul(xs::Vector{T}, ys::Vector{T})::Vector
   vv, shift = Vector{}[], 0
   for x ∈ xs
-    c, v = zero(T), T[]
-    for y ∈ ys
-      m = x * y + c
-      (c, m) = m > T(9) ? divrem(m, T(10)) : (zero(T), m)
-      push!(v, m)
-    end
-    c > zero(T) && push!(v, c) # carry
-    for _ ∈ 1:shift
-      pushfirst!(v, zero(T))
-    end
+    push!(vv, vcat(zeros(T, shift), ys .* x)) ## Using vectorization
     shift += 1
-    push!(vv, v)
-    c = zero(T) # reset carry
   end
   vv
 end
@@ -107,3 +81,41 @@ function find_ix(s::String; ch = '.')
   end
   nothing
 end
+
+## alternative mul. w/o vectorization
+function mul_(xs::Vector{T}, ys::Vector{T})::Vector
+  vv, shift = Vector{}[], 0
+  for x ∈ xs
+    c, v = zero(T), T[]
+    for y ∈ ys
+      m = x * y + c
+      (c, m) = m > T(9) ? divrem(m, T(10)) : (zero(T), m)
+      push!(v, m)
+    end
+    c > zero(T) && push!(v, c) # carry
+    for _ ∈ 1:shift
+      pushfirst!(v, zero(T))
+    end
+    shift += 1
+    push!(vv, v)
+    c = zero(T) # reset carry
+  end
+  vv
+end
+
+##
+## Using BigInt makes it too easy (and why using string then?)
+##
+# function mul_cheat(s₁::String, s₂::String)::String
+#   r = 0
+#   s₁, s₂ = s₁ < s₂ ? (s₁, s₂) : (s₂, s₁)
+#   xs = split(s₁, "") |> v -> parse.(BigInt, v)
+#   y = parse(BigInt, s₂)
+#   p = 0
+#   for x ∈ xs |> reverse
+#     sm  = string(y * x, "0"^p)
+#     r += parse(BigInt, sm)
+#     p += 1
+#   end
+#   string(r)
+# end
