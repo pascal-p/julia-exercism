6a6c84c7eb21bbbe4a2402fc8a50c21ef0ee1caf
Pascal P <lacsap_666@yahoo.fr>
2022-12-31 20:27:24 +1300

    rail-cipher: iter 2 - encode base

--- kata/rail-cipher/rail-cipher.jl
+++ kata/rail-cipher/rail-cipher.jl
@@ -1,7 +1,28 @@
 const N = 3 # number of rails
 
-function encode(s::String)::String
+function encode(s::String; n = N)::String
+  @assert n ≥ 2
+  rails = fill("", n)
+  offset = 1
+  for (ix, seq) ∈ enumerate(sequences(n=n))
+    k, iseq = offset, 1
+    while k ≤ length(s)
+      # println("offset: $(offset) / lim: $(length(s) - offset + 1) / ix: $(ix) / seq: $(seq) / s[k:$(k)]: $(s[k])")
+      rails[ix] = string(rails[ix], s[k])
+      seq[iseq] == 0 && (iseq = 3 - iseq)
+      k += seq[iseq]
+      iseq = 3 - iseq
+    end
+    offset += 1
+  end
+  rails |> v -> join(v, "")
 end
 
-function decode(s::String):: String
+function decode(s::String; n = N):: String
+end
+
+function sequences(;n = N)
+  ## cf. runtests.jl for examples
+  seed = 2 * n - 2
+  zip(seed:-2:0, 0:2:seed) |> collect
 end
--- kata/rail-cipher/runtests.jl
+++ kata/rail-cipher/runtests.jl
@@ -1,56 +1,31 @@
 using Test
 
-include("fav-num.jl")
+include("rail-cipher.jl")
 
-@testset "base cases" begin
-  @test decompose(4) ∈ [(0, 1, 2)]
-  @test decompose(7) ∈ [(0, 1, 3)]
-  @test decompose(10) ∈ [(1, 2, 3)]
+@testset "encode" begin
+  @test encode("WEAREDISCOVEREDFLEEATONCE") == "WECRLTEERDSOEEFEAOCAIVDEN"
 
-  @test decompose(24) ∈ [(0, 2, 6)]
-  @test decompose(34) ∈ [(0, 3, 7), (2, 4, 6)]
+  @test encode("WEAREDISCOVEREDFLEEATONCE"; n=5) == "WCLEESOFECAIVDENRDEEAOERT"
 
-  @test decompose(69) ∈ [(0, 2, 11)]
-  @test decompose(68) ∈ [(2, 4, 10)]
-  @test decompose(67) ∈ [(0, 1, 11)]
-  @test decompose(66) ∈ [(1, 4, 10)]
-  @test decompose(65) ∈ [(0, 4, 10)]
+  @test encode("THEDEVILISINTHEDETAILS"; n=5) == "TIEHLSDTEIIEADVNHISETL"
 
-  @test decompose(70) ∈ [(1, 2, 11)]
-  @test decompose(72) ∈ [(0, 3, 11)]
-
-  @test decompose(101) ∈ [(0, 4, 13)]
-  @test decompose(111) ∈ [(0, 3, 14)]
-
-  @test decompose(2010) ∈ [(3, 26, 57)]
-
-  @test decompose(10101) ∈ [(0, 21, 140)]
+  @test encode("XOXOXOXOXOXOXOXOXO"; n=2) == "XXXXXXXXXOOOOOOOOO"
 end
 
 @testset "more cases" begin
-  @test decompose(10_000_0000) ∈ [(12, 133, 14141)]
-
-  @test decompose(100_000_0000) ∈ [(18, 1332, 44701)]
-
-  @test decompose(100_000_000_000) ∈ [(44, 2690, 447205)]
-  @test decompose(1_000_000_000_000_000_000) ∈ [(1518, 1693417, 1414212548)]
+  # @test
 end
 
 @testset "Exception" begin
-  # for number too small to be expressed in this base!
-  for i ∈ 0:3
-    @test_throws AssertionError decompose(i)
-  end
+  @test_throws AssertionError encode("One rail, only one rail"; n=1) == "One rail, only one rail"
+end
 
-  for i ∈ 5:6
-    @test_throws AssertionError decompose(i)
-  end
+@testset "sequences" begin
+  @test sequences(n = 2) == [(2, 0), (0, 2)]
 
-  @test_throws AssertionError decompose(8)
+  @test sequences(n = 4) == [(6, 0), (4, 2), (2, 4), (0, 6)]
 
-  @test_throws AssertionError decompose(12)
+  @test sequences(n = 5) == [(8, 0), (6, 2), (4, 4), (2, 6), (0, 8)]
 
-  @test_throws AssertionError decompose(15)
-
-  @test_throws AssertionError decompose(20)
+  @test sequences(n = 3) == [(4, 0), (2, 2), (0, 4)]
 end
