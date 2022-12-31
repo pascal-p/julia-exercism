9979dc6c830c2f949d926b1753f998b8c5bf06e0
Pascal P <lacsap_666@yahoo.fr>
2023-01-01 12:36:43 +1300

    vigenere iter 2: macro

--- kata/vig-cipher/vig-cipher.jl
+++ kata/vig-cipher/vig-cipher.jl
@@ -7,10 +7,29 @@ const CODE2LETTER = Dict{Int8, Char}(
   i => l for (i, l) ∈ enumerate('A':'Z' |> collect)
 )
 
-function encode(s::AbstractString, key::AbstractString)::AbstractString
-  klen, slen = length(key), length(s)
-  nkey = length(key) < length(s) ? complement(key, slen - klen, klen) : key
+macro compkey(fn::Expr)
+  local fn_sign = fn.args[1].args  # == Any[:(encode(s::AbstractString, key::AbstractString)), :AbstractString]
 
+  # fn_sign[1] = :(encode(s::AbstractString, key::AbstractString)) == Expr
+  #           .args[1] == :encode
+  #           .args[2] == s::AbstractString
+  #           .args[3] == key::AbstractString
+  local s, key = (fn_sign[1].args[2].args[1], fn_sign[1].args[3].args[1])
+
+  ## replace body which is fn.args[2]
+  fn.args[2] = quote
+    # inject the check
+    klen, slen = length(key), length(s)
+    nkey = length(key) < length(s) ? complement(key, slen - klen, klen) : key
+
+    # copy back the original function body
+    $(fn.args[2])
+  end
+  fn
+end
+
+@compkey function encode(s::AbstractString, key::AbstractString)::AbstractString
+  # slen, nkey injected by macro
   ciphered = fill("", slen) # pre-alloc
   for (ix, (l, k)) in enumerate(zip(s, nkey))
     ciphered[ix] = CODE2LETTER[(LETTER2CODE[l] + LETTER2CODE[k] - 1) % 26] |> string
@@ -19,10 +38,8 @@ function encode(s::AbstractString, key::AbstractString)::AbstractString
   join(ciphered, "")
 end
 
-function decode(s::AbstractString, key::AbstractString)::AbstractString
-  klen, slen = length(key), length(s)
-  nkey = length(key) < length(s) ? complement(key, slen - klen, klen) : key
-
+@compkey function decode(s::AbstractString, key::AbstractString)::AbstractString
+  # slen, nkey injected by macro
   deciphered = fill("", slen) # pre-alloc
   for (ix, (l, k)) in enumerate(zip(s, nkey))
     deciphered[ix] = CODE2LETTER[(LETTER2CODE[l] - LETTER2CODE[k] + 27) % 26] |> string
