b6f999dfb4e6e798e5012b5ccc571698284c3ce8
Pascal P <lacsap_666@yahoo.fr>
2022-12-31 20:39:14 +1300

    rail-cipher: iter 3 - decode base

--- kata/rail-cipher/rail-cipher.jl
+++ kata/rail-cipher/rail-cipher.jl
@@ -19,6 +19,20 @@ function encode(s::String; n = N)::String
 end
 
 function decode(s::String; n = N):: String
+  ds = fill("", length(s)) # repeat(" ", length(s))
+  ix, offset = 1, 1
+  for seq ∈ sequences(n=n)
+    k, iseq = offset, 1
+    while k ≤ length(s)
+      ds[k] = string(s[ix])
+      seq[iseq] == 0 && (iseq = 3 - iseq)
+      k += seq[iseq]
+      iseq = 3 - iseq
+      ix += 1
+    end
+    offset += 1
+  end
+  ds |> v -> join(v, "")
 end
 
 function sequences(;n = N)
@@ -26,3 +40,13 @@ function sequences(;n = N)
   seed = 2 * n - 2
   zip(seed:-2:0, 0:2:seed) |> collect
 end
+
+# julia> decode("WECRLTEERDSOEEFEAOCAIVDEN")
+# ERROR: MethodError: no method matching setindex!(::String, ::Char, ::Int64)
+# Stacktrace:
+#  [1] decode(s::String; n::Int64)
+#    @ Main ~/Projects/Exercism/julia/kata/rail-cipher/rail-cipher.jl:28
+#  [2] decode(s::String)
+#    @ Main ~/Projects/Exercism/julia/kata/rail-cipher/rail-cipher.jl:21
+#  [3] top-level scope
+#    @ REPL[2]:1
