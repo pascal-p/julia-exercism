41cd85d8b22516be5d31584fb267599e79718f9a
Pascal P <lacsap_666@yahoo.fr>
2023-01-05 21:49:00 +1300

    eval-arith-expr, iter1 - the basic

---
+++ kata/eval-arith-expr/eval-arith-expr.jl
@@ -0,0 +1,104 @@
+using Base: start_reading
+const OPS = ['+', '-', '*', '/']
+import Base: +, -, *, /
+
+const DIGITS = '0':'9'
+
+const OPER_MAP = Dict{Symbol, Function}(
+  :+ => +,
+  :- => -,
+  :* => *,
+  :/ => /,
+)
+
+const DEBUG = false
+
+function parseexpr(expr::String)
+  cstate, pstate = :start, :start
+  operand_stack, operator_stack = [], []
+  token, oper = "", ""
+  forbiden = '_'
+
+  for ch ∈ strip(expr)
+    DEBUG && println(" ", ch, " ", typeof(ch))
+
+    ch == forbiden && throw(ArgumentError("space not allowed at this position"))
+    forbiden = '_'
+
+    if ch ∈ DIGITS
+      pstate = cstate
+      cstate = :number
+      token = string(token, ch)
+
+    elseif ch == ' '
+      pstate = cstate
+      # cstate = :space # DO NOTY CHANGE current state
+      if pstate == :number
+        # we finish reading a number
+        pushfirst!(operand_stack, token |> parse_num)
+        token = "" # reset
+      end
+
+    elseif ch ∈ OPS
+      if ch == '-' && pstate == :start
+        # not an operator, but sign
+        DEBUG && println("set forbidden char...")
+        forbiden = ' ' # we do not want space
+        token = string(ch) # not an operator, but sign
+        continue
+      end
+
+      pstate = cstate
+      cstate = :operator
+      if pstate == :operator # || pstate == :space
+        if ch == '-'
+          DEBUG && println("set forbidden char...")
+          forbiden = ' ' # we do not want space
+          token = string(ch) # not an operator, but sign
+          continue
+        end
+      elseif pstate == :number && token != ""
+        # we finish reading a number
+        pushfirst!(operand_stack, token |> parse_num)
+        token = "" # reset
+      end
+      oper = string(ch)
+      pushfirst!(operator_stack, Symbol(oper))
+
+    elseif ch == '('
+      # TODO
+
+    elseif ch == ')'
+      # TODO
+
+    end
+  end
+  DEBUG && println("[exit loop] operands: $(operand_stack) / operators: $(operator_stack)")
+
+  if cstate == :number # pstate == :number || cstate == :number
+    DEBUG && println("[left?] token: ", token)
+    pushfirst!(operand_stack, token |> parse_num)
+  end
+
+  DEBUG && println("operands: $(operand_stack) / operators: $(operator_stack)")
+
+  while !isempty(operator_stack)
+    oper = pop!(operator_stack)
+    x = pop!(operand_stack)
+    y = pop!(operand_stack)
+    push!(operand_stack, OPER_MAP[oper](x, y))
+  end
+
+  DEBUG && println("results: $(operand_stack[1])")
+  pop!(operand_stack)
+end
+
+function parse_num(token::String)
+  # sign (nothing to do) and dot
+  if occursin(".", token)
+    # decimal case
+    parse(Float32, token)
+  end
+  parse(Int, token)
+end
+
---
+++ kata/eval-arith-expr/runtests.jl
@@ -0,0 +1,20 @@
+using Test
+
+include("eval-arith-expr.jl")
+
+@testset "arithmetic evaluation base cases" begin
+  @test parseexpr(" 2* -1 + -3") == -5
+  @test parseexpr(" 2* 1 + -3") == -1
+  @test parseexpr(" -2* 1 + -3") == -5
+  @test parseexpr(" -2* 1 + 3") == 1
+  @test parseexpr(" -2* -1 + 3") == 5
+end
+
+@testset "arithmetic evaluation special casses" begin
+end
+
+@testset "exception" begin
+  @test_throws ArgumentError parseexpr(" -2* - 1 + 3")
+  @test_throws ArgumentError parseexpr(" -2* -1 + - 3")
+  @test_throws ArgumentError parseexpr(" 1- - 1")
+end
