30c77325315052e123d15ca0334af820927ff612
Pascal P <lacsap_666@yahoo.fr>
2023-01-06 16:17:49 +1300

    eval-arith-expr, iter4 parenthesized exprs

--- kata/eval-arith-expr/eval-arith-expr.jl
+++ kata/eval-arith-expr/eval-arith-expr.jl
@@ -1,4 +1,4 @@
-import Base: +, -, *, /
+import Base: +, -, *, /, pushfirst!
 
 const OPS = ['+', '-', '*', '/']
 const DIGITS = '0':'9'
@@ -16,10 +16,9 @@ function parseexpr(expr::String)
   cstate, pstate = :start, :start
   operand_stack, operator_stack = [], Symbol[]
   token, oper, forbiden = "", "", '_'
+  porder, order = nothing, :l2r # :r2l
 
   for ch ∈ strip(expr)
-    DEBUG && println(" ", ch, " ", typeof(ch))
-
     ch == forbiden && throw(ArgumentError("space not allowed at this position"))
     forbiden = '_'
 
@@ -40,13 +39,13 @@ function parseexpr(expr::String)
 
     if ch == ' '
       pstate = cstate
-      # cstate = :space # DO NOT CHANGE current state
-      pstate == :number && token != "" && (token = number!(operand_stack, token))
+      # DO NOT CHANGE current state
+      pstate == :number && token != "" && (token = number!(operand_stack, token, order))
       continue
     end
 
     if ch ∈ OPS
-      if ch == '-' && pstate == :start # not an operator, but sign
+      if ch == '-' && pstate ∈ (:start, :operator) # not an operator, but sign
         forbiden, token = sign(ch)
         continue
       end
@@ -54,71 +53,147 @@ function parseexpr(expr::String)
       pstate = cstate
       cstate = :operator
 
-      if pstate == :operator && ch == '-' # not an operator, but sign
-        forbiden, token = sign(ch) # do we need to change the state back?
-        continue
-      end
-
-      pstate == :number && token != "" && (token = number!(operand_stack, token))
+      pstate == :number && token != "" && (token = number!(operand_stack, token, order))
       oper = string(ch)
-      pushfirst!(operator_stack, Symbol(oper))
+      pushfirst!(operator_stack, Symbol(oper), order)
       continue
     end
 
     if ch == '('
-      # TODO
+      order == :l2r && ((porder, order) = (:l2r, :r2l))
+      pstate == :number && throw(ArgumentError("Arithmetic expression not well formed :("))
+
+      pstate = cstate
+      cstate = :open_par
+
+      token = string(token, ch)
+      Base.pushfirst!(operand_stack, token)
+      Base.pushfirst!(operator_stack, Symbol(token))
+      token = ""
+
       continue
     end
 
     if ch == ')'
-      # TODO
+      "(" ∉ operand_stack && throw(ArgumentError("Arithmetic expression not well formed :)"))
+
+      cstate == :number && length(string(token)) ≥ 1 && (token = number!(operand_stack, token, order))
+      # need to eval expr up to first matching '('
+      evalexpr(operator_stack, operand_stack; limit='(')
+
+      order == :r2l && ((porder, order) = (nothing, :l2r))
+      pstate = cstate
+      cstate = :close_par # :number  # akin to number state...
       continue
     end
   end
 
-  DEBUG && println("[exit loop] operands: $(operand_stack) / operators: $(operator_stack)")
-
-  if cstate == :number # pstate == :number || cstate == :number
-    DEBUG && println("[left?] token: ", token)
-    pushfirst!(operand_stack, token |> parse_num)
+  if cstate == :number
+    DEBUG && println("[left?] token: <$(token)>")
+    pushfirst!(operand_stack, token |> parse_num, order)
   end
 
-  DEBUG && println("operands: $(operand_stack) / operators: $(operator_stack)")
   evalexpr(operator_stack, operand_stack)
-
-  DEBUG && println("results: $(operand_stack[1])")
   pop!(operand_stack)
 end
 
-function number!(operand_stack, token)
-  # we finish reading a number
-  pushfirst!(operand_stack, token |> parse_num)
+function pushfirst!(operator_stack::Vector{Symbol}, token::Symbol, order::Symbol) # for operator
+  if order == :l2r
+    Base.pushfirst!(operator_stack, token)
+    return
+  end
+
+  if operator_stack[1] == Symbol("(")
+    Base.pushfirst!(operator_stack, token)
+  else
+    ## Insert at left pos of leftmost Symbol("(")
+    ix = find_index(Symbol("("), operator_stack)
+    push_at!(operator_stack, ix - 1, token)
+  end
+end
+
+function pushfirst!(operand_stack::Vector{Any}, token::Union{Number, String}, order::Symbol) # for operands
+  if order == :l2r
+    Base.pushfirst!(operand_stack, token)
+    return
+  end
+
+  if operand_stack[1] == "("
+    Base.pushfirst!(operand_stack, token)
+  else
+    ## Insert at left pos of leftmost "("
+    ix = find_index("(", operand_stack)
+    push_at!(operand_stack, ix - 1, token)
+  end
+end
+
+function number!(operand_stack, token, order)  # we finish reading a number
+  pushfirst!(operand_stack, token |> parse_num, order)
   token = "" # reset
 end
 
 function sign(ch::Char)
-  DEBUG && println("set forbidden char...")
-  forbiden = ' ' # we do not want space
+  forbiden = ' '     # we do not want space
   token = string(ch) # not an operator, but sign
   (forbiden, token)
 end
 
-function parse_num(token::String)
+function parse_num(token::String)::Number
   # sign (nothing to do) and dot
-  if occursin(".", token)
-    return parse(TT, token) # decimal case
-  end
+  occursin(".", token) && (return parse(TT, token)) # decimal case
   parse(Int, token)
 end
 
-function evalexpr(operator_stack::Vector{Symbol}, operand_stack::Vector)
+function evalexpr(operator_stack::Vector{Symbol}, operand_stack::Vector; limit=nothing)
   while !isempty(operator_stack)
-    oper = pop!(operator_stack)
-    x = pop!(operand_stack)
-    y = pop!(operand_stack)
+    if length(operator_stack) == 1 && operator_stack[1] == Symbol("(") && length(operand_stack) == 1
+      pop!(operator_stack)
+      break
+    end
+
+    (oper, x, y) = getops!(operator_stack, operand_stack, limit === nothing ? pop! : popfirst!)
+
     iszero(y) && oper == :/ && (throw(DivideError())) # in this restricted case only division by zero is problematic
     oper == :/ && ((x, y) = (TT(x), TT(y)))
+    r = OPER_MAP[oper](x, y)
+    if limit !== nothing
+      if !isempty(operand_stack) && operand_stack[1] == "("
+        operand_stack[1] = r
+        break
+      end
+      Base.pushfirst!(operand_stack, r)
+      continue
+    end
 
-    push!(operand_stack, OPER_MAP[oper](x, y))
+    Base.push!(operand_stack, r)
   end
+end
+
+function getops!(operator_stack, operand_stack, popoper!)
+  oper = popoper!(operator_stack)
+
+  # oper == Symbol("(") && (oper = popoper!(operator_stack)) ## ASSUME operator_stack is NOT of length 0
+  if oper == Symbol("(")
+    @assert !isempty(operator_stack)
+    oper = popoper!(operator_stack)
+  end
+
+  x = popoper!(operand_stack)
+  y = popoper!(operand_stack)
+  (oper, x, y)
+end
+
+function find_index(e::Union{String, Symbol}, stack::Vector)
+  for ix ∈ 1:length(stack)
+    stack[ix] == e && return ix
+  end
+  nothing
+end
+
+function push_at!(stack::Vector, ix, token)
+  Base.pushfirst!(stack, :_) # placeholder
+  for jx ∈ 1:ix              # push to left
+    stack[jx] = stack[jx + 1]
+  end
+  stack[ix + 1] = token      # insert
 end
--- kata/eval-arith-expr/runtests.jl
+++ kata/eval-arith-expr/runtests.jl
@@ -13,6 +13,11 @@ include("eval-arith-expr.jl")
   @test parseexpr(" -2* 1 + -3") == -5
   @test parseexpr(" -2* 1 + 3") == 1
   @test parseexpr(" -2* -1 + 3") == 5
+
+  @test parseexpr("2 * 5 * -3 * 7 + -2") == -212
+
+  @test parseexpr("2 + 3 * 4") == 20 # evaluation uses left associativity (of the operators)
+  @test parseexpr("2 * 3 + 4") == 10 # evaluation uses left associativity (of the operators)
 end
 
 @testset "arithmetic evaluation with decimal" begin
@@ -34,14 +39,36 @@ end
 end
 
 @testset "parenthesized arithmetic evaluation" begin
-  # TBD...
+  @test parseexpr("2 * (1 + 3)") == 8
+  @test parseexpr("( 1 - 1 )") == 0
+  @test parseexpr("2 * (1 + 3 + -2)") == 4
+  @test parseexpr("2 * (1 + 3 + -2) - 2") == 2
+  @test parseexpr("2 * (1 + 3 + -2) - (2 + 2)") == 0
+
+  @test parseexpr("2 * (1 + (3 * -2))") == -10
+  @test parseexpr("2 * (1 + (3 * -2) + 2)") == -6
+  @test parseexpr("2 * (1 + (3 * -2) + 2) + 6") == 0
+
+  @test parseexpr("2. * (1 + (3. * -2) + 2) + 6") == 0.0f0
+  @test parseexpr("2. * (1 + (3 * -2) + 2.)") == -6.0f0
+
+  # associativity is left to right
+  @test parseexpr("2 * (2 * -3 * 7 + -4)") == -92  # inside parentheses left to right rule applies (unless modified by another set of parentheses)
+
+  @test parseexpr("2.1 * (1.5 * -3.4 * 7.1 + -2.89)") ≈ -82.11f0
+  @test parseexpr("   2.1 *      ( 1.5  * -3.4 * 7.1 + -2.89 )  ") ≈ -82.11f0
+
+  @test parseexpr("2 + (3 * 4)") == 14 # change default left to right assoc rules
+  @test parseexpr("(2 + 3) * 4") == 20 # left to right assoc rule -> parentheses needless
 end
 
 @testset "exception" begin
   @test_throws ArgumentError parseexpr(" -2* - 1 + 3")
   @test_throws ArgumentError parseexpr(" -2* -1 + - 3")
   @test_throws ArgumentError parseexpr(" 1- - 1")
-
+  @test_throws ArgumentError parseexpr("- 1- - 1 ")
+  @test_throws ArgumentError parseexpr("- 6 + - (4)")
+  @test_throws ArgumentError parseexpr("- 6 + -(- 4)")
   @test_throws DivideError parseexpr(" 1 / 0")
   @test_throws DivideError parseexpr(" 1 / 0.")
 end
