1373df38895533009ebb46b2f3358d055db7f097
Pascal P <lacsap_666@yahoo.fr>
2023-01-30 15:12:02 +1300

    init. rev.

---
+++ kata/symb-diff/runtests.jl
@@ -0,0 +1,14 @@
+using Test
+
+include("symb-diff.jl")
+
+@testset "symbolic differentiation base cases" begin
+end
+
+@testset "symbolic differentiation beyond base  cases" begin
+
+end
+
+@testset "symbolic differentiation invalid expression" begin
+  # @test_throws ArgumentError foo()
+end
