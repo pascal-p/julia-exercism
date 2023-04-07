7fd652d70fac7540f7ded5a5deb36f1d72b45444
Pascal P <lacsap_666@yahoo.fr>
2023-04-08 09:54:08 +1200

    inversion count review

--- Algo/02-counting-inversions/README.md
+++ Algo/02-counting-inversions/README.md
@@ -9,8 +9,8 @@ First, check that your algorithms counts 0 inversions for a sorted array, and n(
 This file (http://www.algorithmsilluminated.org/datasets/problem3.5test.txt) contains 10 integers, representing a 10-element array. Your program should count 28 inversions in this array.
         
 ### Challenge data set: 
-This file (http://www.algorithmsilluminated.org/datasets/problem3.5.txt) contains all of the integers between 1 and 100,000 (inclusive) in some order, with no integer repeated.   
-The ith row of the file indicates the ith entry of an array. How many inversions does this array have?  
+This file (http://www.algorithmsilluminated.org/datasets/problem3.5.txt) contains all of the integers between 1 and 100,000 (inclusive) in some order, with no integer repeated.\
+The ith row of the file indicates the ith entry of an array. How many inversions does this array have?
 
 Obviously, to get the most out of this assignment, you should implement the fast divide-and-conquer algorithm from Section 3.2, rather than brute-force search.) 
 
--- Algo/02-counting-inversions/counting_inversions.jl
+++ Algo/02-counting-inversions/counting_inversions.jl
@@ -1,88 +1,88 @@
-const VReal{T} = Vector{T} where T<: Real
+const VReal{T} = Vector{T} where T <: Real
 
 """
-   counting_inversions(a::VReal)::Tuple{Integer}
+   counting_inversions(v::VReal)::Tuple{Integer}
 
-   input: a array of integer in a random order
-   output: sorted array (a sorted) b and the number of inversions
+input: v array of `Julia` Real number in a random order
+output: the resulting sorted array and the number of inversions
 
-   The running time of this algorithm (using Gauss trick 3 mult. instead of 4) can be found
-   by using the Master Method (parameterized by 3 var. a,b and d) as follows:
+The running time of this algorithm (using Gauss trick 3 mult. instead of 4) can be found
+by using the Master Method (parameterized by 3 var. a, b and d) as follows:
+  - a = 3 (number of recursive calls or sub-problems)
+  - b = 2 (each sub-problems is half the size of the original one)
+    d = 1 (outside the rec. call we need to do some adds and subs, this is done in linear time)
 
-   - a = 3 (number of recursive calls or sub-problems)
-   - b = 2 (each sub-problems is half the size of the original one)
-     d = 1 (outside the rec. call we need to do some addtions and subtractions - which is done in linear
-            time)
-
-     Therefore  a (= 3) > bᵈ (= 2)  => Running time is:
-       O( n^(log₂(3)) ) ≈ O(n¹.⁵⁸)
+Therefore  a (= 3) > bᵈ (= 2)  => Running time is:
+  O(n^(log₂(3))) ≈ O(n¹.⁵⁸)
 """
 
-function counting_inversions(a::VReal)::Tuple{VReal, Integer}
-  n_a = length(a)
+function counting_inversions(v::VReal)::Tuple{VReal, Integer}
+  vlen = length(v)
 
-  function sort_and_count(a::VReal)::Tuple{VReal, Integer}
-    n = length(a)
-    n ≤ 1 && return(a, 0)
+  function sort_and_count(v::VReal)::Tuple{VReal, Integer}
+    n = length(v)
+    n ≤ 1 && return(v, 0)
 
     ## define limits for sub-arrays (high, low / left, right)
     n₂ = n ÷ 2
-    l_lix, h_rix = 1, n
-    h_lix, l_rix = n₂, n₂ + 1
+    low_lix, high_rix = 1, n
+    high_lix, low_rix = n₂, n₂ + 1
 
-    (c, l_inv_cnt) = sort_and_count(a[l_lix:h_lix])
-    (d, r_inv_cnt) = sort_and_count(a[l_rix:h_rix])
+    (c, l_inv_cnt) = sort_and_count(v[low_lix:high_lix])
+    (d, r_inv_cnt) = sort_and_count(v[low_rix:high_rix])
     (b, shift_inv_cnt) = merge_and_count(c, d)
 
     (b, l_inv_cnt + r_inv_cnt + shift_inv_cnt)
   end
 
-  (b, num_inv) = sort_and_count(a)
-  @assert n_a == length(b) "input array $(a), and resulting array $(b) should have same length"
-  return (b, num_inv)
+  (nv, num_inv) = sort_and_count(v)
+  @assert vlen == length(nv) "input array $(v), and resulting array $(nv) should have same length"
+  (nv, num_inv)
 end
 
-function counting_inversions(a::Any)::Tuple{Any, Integer}
-  length(a) == 0 && return (a, 0)
+function counting_inversions(v::Any)::Tuple{Any, Integer}
+  length(v) == 0 && return (v, 0)
 
-  throw(ArgumentError("$(typeof(a)) not managed yet..."))
+  throw(ArgumentError("$(typeof(v)) not managed yet..."))
 end
 
-function merge_and_count(c::VReal, d::VReal)::Tuple{VReal, Integer}
-  ix_lim, jx_lim = length(c), length(d)
+function merge_and_count(vl::VReal, vr::VReal)::Tuple{VReal, Integer}
+  vl_lim, vr_lim = length(vl), length(vr)
   kx, ix, jx, num_inv = 1, 1, 1, 0
-  b = VReal{eltype(c)}(undef, ix_lim + jx_lim)
+  v = VReal{eltype(vl)}(undef, vl_lim + vr_lim)
 
-  ## merge c, d => b
-  for k in 1:(ix_lim + jx_lim)
-    if ix > ix_lim || jx > jx_lim
+  ## Merge vl, vr => v
+  for k ∈ 1:(vl_lim + vr_lim)
+    if ix > vl_lim || jx > vr_lim
       kx = k
       break
     end
 
-    if c[ix] ≤ d[jx]
-      b[k] = c[ix]
+    if vl[ix] ≤ vr[jx]
+      v[k] = vl[ix]
       ix += 1
     else
-      b[k] = d[jx]
+      v[k] = vr[jx]
       jx += 1
-      num_inv += ix_lim - ix + 1
+      num_inv += vl_lim - ix + 1
     end
   end
 
-  ## copy remaining elements of c into b (if any)
-  while ix ≤ ix_lim
-    b[kx] = c[ix]
-    kx += 1
-    ix += 1
+  ## Copy remaining elements of c into b (if any)
+  if ix ≤ vl_lim
+    l_ix = kx + vl_lim - ix
+    v[kx:l_ix] = vl[ix:end]
+    kx = l_ix + 1
   end
 
-  ## copy remaining elements of d into b (if any)
-  while jx ≤ jx_lim
-    b[kx] = d[jx]
-    kx += 1
-    jx += 1
+  ## Copy remaining elements of d into b (if any)
+  if jx ≤ vr_lim
+    l_jx = kx + vr_lim - jx
+    v[kx:l_jx] = vr[jx:end]
+    kx = l_jx + 1 # one(TIn)  # not necessary (because nothing more to do), just
+    # for the sake of symmetry, however if we were to permute this block with
+    # previous one, this would become necessary.
   end
 
-  (b, num_inv)
+  (v, num_inv)
 end
