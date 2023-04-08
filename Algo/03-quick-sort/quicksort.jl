7bce87bc5fc49ae42ebcab482487afb0bc63dcc7
Pascal P <lacsap_666@yahoo.fr>
2023-04-08 20:14:45 +1200

    qsort std - review

--- Algo/03-quick-sort/quicksort.jl
+++ Algo/03-quick-sort/quicksort.jl
@@ -1,81 +1,83 @@
 using Random
 
-"""
-  QuickSort standard, not dealing with ties
-"""
-function quick_sort!(a; shuffle=true, cmp_count=false, pivot="first")
-  length(a) ≤ 1 && return a
+const VT = Vector{<: Real}
+const TI = Integer
 
-  length(a) == 2 &&
-    return a[2] < a[1] ? [a[2], a[1]] : a
+"""
+  quick_sort!(v; ...)
+
+Standard quickSort, not dealing with ties
+"""
+function quick_sort!(v::VT; shuffle=true, cmp_count=false, pivot=:first)
+  length(v) ≤ 1 && return v
+  length(v) == 2 && return v[2] < v[1] ? [v[2], v[1]] : v
 
   nb_cmp = 0
 
-  function qsort!(a, l, r)  # closure
+  function qsort!(v::VT, l::TI, r::TI)  # closure
     l ≥ r && return
 
-    ip = choose_pivot(a, l, r, pivot)  # return index of chosen pivot
-    a[l], a[ip] = a[ip], a[l]          # move pivot in first pos. (at index l)
-    ix = partition(a, l, r)
+    ip = choose_pivot(v, l, r, pivot)  # return index of chosen pivot
+    v[l], v[ip] = v[ip], v[l]          # move pivot in first pos. (at index l)
+    ix = partition(v, l, r)
     nb_cmp += r - l
     #
     # There’s no need to count the comparisons one by one. When there is a
-    # recursive call on a subarray of length m, you can simply add m - 1 to your running
-    # total of comparisons. (Recall that the pivot element is compared to each of the
-    # other m - 1 elements in the subarray in this recursive call.)
+    # recursive call on a subarray of length m, you can simply add m - 1 to
+    # your running total of comparisons. (Recall that the pivot element is
+    # compared to each of the other m - 1 elements in the subarray in this
+    # recursive call.)
     #
-    qsort!(a, l, ix - 1)
-    qsort!(a, ix + 1, r)
+    qsort!(v, l, ix - 1)
+    qsort!(v, ix + 1, r)
   end
 
-  shuffle && Random.shuffle!(a)
-  qsort!(a, 1, length(a))
-  return cmp_count ? (a, nb_cmp) : a
+  shuffle && Random.shuffle!(v)
+  qsort!(v, 1, length(v))
+  cmp_count ? (v, nb_cmp) : v
 end
 
-function partition(a, l, r)
-  pivot = a[l]
+function partition(v::VT, l::TI, r::TI)
+  pivot = v[l]
   ix = l + 1
 
-  for jx in (l + 1):r
-    if a[jx] < pivot
-      a[jx], a[ix] = a[ix], a[jx]
+  for jx ∈ (l + 1):r
+    if v[jx] < pivot
+      v[jx], v[ix] = v[ix], v[jx]
       ix += 1
     end
   end
 
   ix -= 1
-  a[l], a[ix] = a[ix], a[l] # move pivot to its final place in sorted array
-  return ix                 # indice of pivot
+  v[l], v[ix] = v[ix], v[l] # move pivot to its final place in sorted array
+  ix                        # indice of pivot
 end
 
-function choose_pivot(a, l, r, pivot)
-  if pivot == "first"
+function choose_pivot(v::VT, l::TI, r::TI, pivot::Symbol)
+  if pivot == :first
     l
-  elseif pivot == "last"
+  elseif pivot == :last
     r
-  elseif pivot == "random"
-    rand(l:r)
-  elseif pivot == "median-3"
-    median_3(a, l, r)
+  elseif pivot == :random
+    Random.rand(l:r)
+  elseif pivot == :median3
+    median_3(v, l, r)
   else
     throw(ArgumentError("option not managed yet"))
   end
 end
 
-function median_3(a, l, r)
+function median_3(v, l::TI, r::TI)
   s =  (r - l + 1)
   m = (l  - 1) + ceil(Int, s / 2)
-  x, y, z = a[l], a[m], a[r]
-
+  x, y, z = v[l], v[m], v[r]
   if x < y
     y ≤ z && return m # y
     x < z && return r # z, y ≥ z && y > x
     return l          # x
-
-  else # x ≥ y
-    y ≥ z && return m # y
-    x < z && return l # x,  y ≤ z
-    return r          # z
   end
+  # x ≥ y
+  y ≥ z && return m # y
+  x < z && return l # x,  y ≤ z
+  return r          # z
 end
--- Algo/03-quick-sort/runtests_qs_std.jl
+++ Algo/03-quick-sort/runtests_qs_std.jl
@@ -4,9 +4,9 @@ using Random
 include("quicksort.jl")
 
 @testset "qsort basics" begin
-  @test quick_sort!([]) == []
+  @test quick_sort!(Real[]) == []
   @test quick_sort!([10]) == [10]
-  @test quick_sort!(["b", "a"]) == ["a", "b"]
+  @test quick_sort!([2., 1.]) == [1., 2.] # @test quick_sort!(["b", "a"]) == ["a", "b"]
 end
 
 @testset "qsort on small samples w/o repetition" begin
@@ -21,9 +21,9 @@ end
 
   ## 15 elements
   x = [35, 33, 10, 45, 30, 49, 48, 34, 12, 5, 14, 43, 16, 24, 31]
-    sorted_x = [ 5, 10, 12, 14, 16, 24, 30, 31, 33, 34, 35, 43, 45, 48, 49]
+  sorted_x = [ 5, 10, 12, 14, 16, 24, 30, 31, 33, 34, 35, 43, 45, 48, 49]
 
-  @test quick_sort!(x; shuffle=true, pivot="median-3") == sorted_x
+  @test quick_sort!(x; shuffle=true, pivot=:median3) == sorted_x
 end
 
 @testset "1_000 random-sample with repeated values" begin
@@ -33,28 +33,44 @@ end
   @test quick_sort!(x) == sorted_x
 end
 
-@testset "10_000 random-sample with no repeated values" begin
+@testset "10_000 random-sample with no repeated values / median3" begin
   sorted_x = collect(1:10_000)
   x = Random.shuffle(collect(1:10_000))
 
-  @test quick_sort!(x; shuffle=false, pivot="median-3") == sorted_x
+  @test quick_sort!(x; shuffle=false, pivot=:median3) == sorted_x
 end
 
-@testset "100_000 random-sample with no repeated values" begin
+@testset "10_000 random-sample with no repeated values / random" begin
+  sorted_x = collect(1:10_000)
+  x = Random.shuffle(collect(1:10_000))
+
+  @test quick_sort!(x; shuffle=false, pivot=:random) == sorted_x
+end
+
+@testset "100_000 random-sample with no repeated values / median3" begin
   sorted_x = collect(1:100_000)
 
-  for _ in 1:3
+  for _ ∈ 1:3
     x = Random.shuffle(collect(1:100_000))
-    @test quick_sort!(x; shuffle=false, pivot="median-3") == sorted_x
+    @test quick_sort!(x; shuffle=false, pivot=:median3) == sorted_x
+  end
+end
+
+@testset "100_000 random-sample with no repeated values / random" begin
+  sorted_x = collect(1:100_000)
+
+  for _ ∈ 1:3
+    x = Random.shuffle(collect(1:100_000))
+    @test quick_sort!(x; shuffle=false, pivot=:random) == sorted_x
   end
 end
 
 @testset "1_000_000 random-sample with no repeated values" begin
   sorted_x = collect(1:1_000_000)
 
-  for _ in 1:3
+  for _ ∈ 1:3
     x = Random.shuffle(collect(1:1_000_000))
-    @test quick_sort!(x; shuffle=false, pivot="median-3") == sorted_x
+    @test quick_sort!(x; shuffle=false, pivot=:median3) == sorted_x
   end
 end
 
@@ -62,5 +78,5 @@ end
   sorted_x = collect(1:10_000_000)
   x = Random.shuffle(collect(1:10_000_000))
 
-  @test quick_sort!(x; shuffle=false, pivot="median-3") == sorted_x
+  @test quick_sort!(x; shuffle=false, pivot=:median3) == sorted_x
 end
